95f87f377bc613c33cea714f696ca872e3ccbf70
kunzaZa183 <yongvanichthee@gmail.com>
2023-04-04 15:47:25 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,20 +2,6 @@
 using namespace std;
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    string s;
-    map<string, pair<int, int>> mspii;
-    // hi
-    for (int i = 0; i <= 7; i++)
-        for (int j = 0; j <= 7; j++)
-        {
-            string tmp;
-            for (int k = 0; k <= 7; k++)
-                if (k + 1 == j || k + 1 == i || k - 1 == i || k - 1 == j)
-                    tmp.push_back('0');
-                else
-                    tmp.push_back('1');
-            mspii[tmp] = {i, j};
-        }
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 }
