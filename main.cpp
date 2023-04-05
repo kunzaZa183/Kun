bf4f03896bfc4531e3b89211ad9ec20e10463821
kunzaZa183 <yongvanichthee@gmail.com>
2023-04-05 20:28:29 +0700

    April 5th

--- main.cpp
+++ main.cpp
@@ -4,5 +4,14 @@ int main()
 {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+  int n,m,q;
+  cin>>n>>m>>q;
+  vector<int> adjlist(n);
+  while(m--)
+  {
+    int a,b;
+    cin>>a>>b;
+    a--,b--;
+    
+  }
 }
