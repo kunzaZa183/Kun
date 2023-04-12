9e63b0df76e59a937fa3c4721c1d2f655355c618
kunzaZa183 <yongvanichthee@gmail.com>
2023-04-12 16:51:19 +0700

    Sort 5 Sleep

--- main.cpp
+++ main.cpp
@@ -1,12 +1,43 @@
+#include "sort_weight.h"
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+void sort_init()
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  freopen("grader4.txt", "r", stdin);
-  freopen("out.txt", "w", stdout);
-  int N, K;
-  scanf("%d %d", &N, &K);
-  printf("%d %d\n",N,K);
+  vector<int> vi;
+  int x = sort_weight(0, 1);
+  if (x == -1)
+    vi.push_back(0), vi.push_back(1);
+  if (x == 1)
+    vi.push_back(1), vi.push_back(0);
+  for (int i = 0; i < 2; i++)
+  {
+    x = sort_weight(2, vi[i]);
+    if (x == -1)
+    {
+      vi.insert(vi.begin() + i, 2);
+      break;
+    }
+  }
+  x = sort_weight(3, 4);
+  vector<int> vi2;
+  if (x == -1)
+    vi2.push_back(3), vi2.push_back(4);
+  else
+    vi2.push_back(4), vi2.push_back(3);
+  x = sort_weight(vi2[0], vi[0]);
+  if (x == -1)
+  {
+    vi.insert(vi.begin(), vi2[0]);
+    x = sort_weight(vi[2], vi2[1]);
+    if (x == -1)
+    {
+      x = sort_weight(vi[3], vi2[1]);
+      if (x == -1)
+        vi.push_back(vi2[1]);
+      else if (x == 1)
+        vi.insert(vi.begin() + 3, vi2[1]);
+    }
+    else if(x==1)
+
+  }
 }
---
+++ sort_weight.h
@@ -0,0 +1,16 @@
+int get_case()
+{
+
+}
+void sort_init()
+{
+
+}
+int sort_weight(int lid,int rid)
+{
+
+}
+void sort_answer(int a1,int a2,int a3,int a4,int a5)
+{
+  
+}
