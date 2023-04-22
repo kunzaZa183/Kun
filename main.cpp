138d94195832e05fd315d00e5a545ffdc1db928b
kunzaZa183 <yongvanichthee@gmail.com>
2023-04-22 15:32:27 +0700

    Pointsonstrip

--- main.cpp
+++ main.cpp
@@ -1,43 +1,68 @@
-#include "sort_weight.h"
+// #include "pointlib.h"
 #include <bits/stdc++.h>
 using namespace std;
-void sort_init()
-{
-  vector<int> vi;
-  int x = sort_weight(0, 1);
-  if (x == -1)
-    vi.push_back(0), vi.push_back(1);
-  if (x == 1)
-    vi.push_back(1), vi.push_back(0);
-  for (int i = 0; i < 2; i++)
-  {
-    x = sort_weight(2, vi[i]);
-    if (x == -1)
-    {
-      vi.insert(vi.begin() + i, 2);
-      break;
-    }
-  }
-  x = sort_weight(3, 4);
-  vector<int> vi2;
-  if (x == -1)
-    vi2.push_back(3), vi2.push_back(4);
-  else
-    vi2.push_back(4), vi2.push_back(3);
-  x = sort_weight(vi2[0], vi[0]);
-  if (x == -1)
-  {
-    vi.insert(vi.begin(), vi2[0]);
-    x = sort_weight(vi[2], vi2[1]);
-    if (x == -1)
-    {
-      x = sort_weight(vi[3], vi2[1]);
-      if (x == -1)
-        vi.push_back(vi2[1]);
-      else if (x == 1)
-        vi.insert(vi.begin() + 3, vi2[1]);
-    }
-    else if(x==1)
 
+static int n;
+static int x[10010];
+static int y[10010];
+
+int point_init()
+{
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++)
+    scanf("%d %d", &x[i], &y[i]);
+  return n;
+}
+
+int query(int i, int j)
+{
+  return ((x[i] > x[j] ? x[i] - x[j] : x[j] - x[i]) +
+          (y[i] > y[j] ? y[i] - y[j] : y[j] - y[i]));
+}
+
+int main()
+{
+  int n = point_init();
+  int x = query(0, 1);
+  if (n == 2)
+  {
+    cout << "0 0\n";
+    cout << x - 1 << " 1\n";
+    return 0;
+  }
+  vector<pair<int, int>> vpii(n, {INT_MAX, INT_MAX});
+  vpii[0] = {0, 0};
+  for (int i = 2; i < n; i++)
+  {
+    int y = query(1, i), z = query(0, i);
+    if (x > y && x > z)
+    {
+      if (y + z == x)
+      {
+        vpii[1] = {x, 0};
+        vpii[i] = {z, 0};
+      }
+      else if (y + z == x + 2)
+      {
+        vpii[1] = {x, 0};
+        vpii[i] = {z - 1, 1};
+      }
+      else if (y + z == x + 1)
+        vpii[1] = {x - 1, 1};
+    }
+    else if (y > x && y > z)
+    {
+      // assume that vpii[1] is to the right
+      if (x + z == y)
+      {
+        vpii[1] = {x, 0};
+        vpii[i] = {-z, 0};
+      }
+      else if(x+z==y+1)
+      {
+        .
+        
+      }
+    }
   }
 }
