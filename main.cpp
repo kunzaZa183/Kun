83d7d79d55bd5ac76b9f84d48dd24ed6a856c0cf
kunzaZa183 <yongvanichthee@gmail.com>
2023-06-17 15:44:38 +0700

    Random Commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct trie
+{
+  int depth;
+  trie *l, *r;
+  trie()
+  {
+    depth = 0;
+    l = nullptr, r = nullptr;
+  }
+} *root;
+void addone(trie *cur, vector<int> &add)
+{
+  if (cur->depth == 30)
+    return;
+  if (add[cur->depth] == 0)
+  {
+    if (cur->l == nullptr)
+    {
+      cur->l = new trie;
+      cur->l->depth = cur->depth + 1;
+    }
+    addone(cur->l, add);
+  }
+  else if (add[cur->depth] == 1)
+  {
+    if (cur->r == nullptr)
+    {
+      cur->r = new trie;
+      cur->r->depth = cur->depth + 1;
+    }
+    addone(cur->r, add);
+  }
+}
+pair<int, vector<vector<int> *> *> smalltolarge(trie *cur)
+{
+  vector<vector<int> *> *ret = new vector<vector<int> *>;
+  if (cur == nullptr)
+    return {0, ret};
+  
+}
 int main()
 {
   ios::sync_with_stdio(0);
