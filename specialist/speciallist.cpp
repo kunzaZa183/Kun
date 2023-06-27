4477021887819ed6fe974d33b0429ebce923eefe
kunzaZa183 <yongvanichthee@gmail.com>
2023-06-27 15:14:13 +0700

    Specialist

---
+++ specialist/speciallist.cpp
@@ -0,0 +1,11 @@
+#include "speciallist.h"
+#include <vector>
+
+std::vector<int> compute_cost(int N,
+			      int M,
+			      std::vector<int> P,
+			      std::vector<int> X,
+			      std::vector<int> Z)
+{
+  return {0};
+}
