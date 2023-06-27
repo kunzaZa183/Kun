53a0f10955df3026549f835478a9c892eda207e0
kunzaZa183 <yongvanichthee@gmail.com>
2023-06-27 10:35:20 +0700

    Socks

--- main.cpp
+++ main.cpp
@@ -22,40 +22,51 @@ int recurdp(int in, int l)
   return dp[in][l];
 }
 vector<int> cur;
-void findans(int in, int l, int &k)
+void findans(int in, int l, int k)
 {
   if (l == -1)
     return;
-  if (k == 0)
+  vector<pair<int, int>> order; // value in
+  for (int i = in + 1; i < dp.size(); i++)
+    order.push_back({ vi[i], i });
+  sort(order.begin(), order.end());
+  if (k == 0 && in != 0)
   {
     for (auto a : cur)
       cout << a << ' ';
     cout << '\n';
     exit(0);
   }
-  if (k >= dp[in][l])
-  {
-    k -= dp[in][l];
-    return;
-  }
-  vector<pair<int, int>> order; // value in
-  for (int i = in + 1; i < dp.size(); i++)
-    order.push_back({vi[i], i});
-  sort(order.begin(), order.end());
+  k--;
   for (auto a : order)
     if (vi[in] < a.first)
-      findans(a.second, l, k);
-    else if(vi[in]>a.first)
-      findans(a.second,l-1,);
-
+    {
+      if (k < recurdp(a.second, l))
+      {
+        cur.push_back(a.first);
+        findans(a.second, l, k);
+      }
+      else
+        k -= recurdp(a.second, l);
+    }
+    else if (vi[in] > a.first)
+    {
+      if (k < recurdp(a.second, l - 1))
+      {
+        cur.push_back(a.first);
+        findans(a.second, l - 1, k);
+      }
+      else
+        k -= recurdp(a.second, l - 1);
+    }
 }
-int main()
+int32_t main()
 {
   ios::sync_with_stdio(0);
   cin.tie(0);
   int n, l, k;
   cin >> n >> l >> k;
-  vi.resize(n, -1);
+  vi.resize(n + 1, -1);
   for (int i = 1; i <= n; i++)
     cin >> vi[i];
   if (k == 0)
@@ -64,9 +75,17 @@ int main()
     return 0;
   }
   dp.resize(n + 1, vector<int>(l + 1, -1));
-  if (k >= dp[0][l])
-  {
-    cout << "-1\n";
-    return 0;
-  }
+  vector<pair<int, int>> order;
+  for (int i = 1; i <= n; i++)
+    order.push_back({ vi[i], i });
+  sort(order.begin(), order.end());
+  for (int i = 0; i < order.size(); i++)
+    if (recurdp(order[i].second, l) <= k)
+      k -= recurdp(order[i].second, l);
+    else
+    {
+      cur.push_back(order[i].first);
+      findans(order[i].second, l, k);
+    }
+  cout << "-1\n";
 }
---
+++ socks/socks.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include "sockslib.h"
+
+using namespace std;
+
+int main()
+{
+  int n;
+  n = num();
+  vector<int> v = {1, 2, 3};
+  cout << ask(v) << endl;
+  match(1, 2);
+}
