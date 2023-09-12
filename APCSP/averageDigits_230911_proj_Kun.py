b85f11c22d372b8287bae105ab217dcc97aa0282
kunzaZa183 <yongvanichthee@gmail.com>
2023-09-12 17:30:15 +0700

    APCSP HW 12 Sep

---
+++ APCSP/averageDigits_230911_proj_Kun.py
@@ -0,0 +1,23 @@
+def count(num):
+  ct = 0
+  numofdig = 0
+  while num > 0:
+    ct += num % 10
+    numofdig += 1
+    num //= 10
+  return ct / numofdig
+
+print(count(234))
+print(count(10000))
+print(count(111))
+print(count(9005))
+print(count(84645))
+print()
+print(count(8547))
+print(count(123456789))
+print(count(5555672468))
+print(count(2548522125455))
+print(count(2545588514548))
+print()
+print(count(111111111))
+print(count(121212121212))
--- main.cpp
+++ main.cpp
@@ -4,41 +4,5 @@ int main()
 {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  string s;
-  cin >> s;
-  char c = s.front();
-  switch (c)
-  {
-  case 't':
-    cout << 3858;
-    break;
-  case 'k':
-    cout << 3679;
-    break;
-  case 'B':
-    cout << 3658;
-    break;
-  case 'U':
-    cout << 3648;
-    break;
-  case 'a':
-    cout << 3638;
-    break;
-  case 'S':
-    cout << 3630;
-    break;
-  case 'e':
-    cout << 3613;
-    break;
-  case 'm':
-    cout << 3555;
-    break;
-  case 'n':
-    cout << 3516;
-    break;
-  case 's':
-    cout << 3481;
-    break;
-  }
-  cout<<endl;
+  
 }
