783854f716b1bba41e7f30b7620ebee8229b68dc
kunzaZa183 <yongvanichthee@gmail.com>
2023-09-13 21:40:31 +0700

    13 Sep
    Didn't finish APCSP Files
    Dont forget to rename

---
+++ APCSP/loopTriangle_230913_proj_Kun.py
@@ -0,0 +1,11 @@
+def triangle(num):
+    for i in range(1, num + 1):
+        print("*" * i)
+    print()
+
+triangle(1)
+triangle(2)
+triangle(3)
+triangle(4)
+triangle(9)
+triangle(12)
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import math
 
 num = 1
 for i in range(1, 1069):
-  num *= i
+    num *= i
 print(math.log10(num))
 
 num2 = 1
 for i in range(500):
-  num2 *= 350001
+    num2 *= 350001
 print(math.log10(num2))
