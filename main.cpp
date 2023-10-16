83b0e8590091bb24bddf7806eb5681511265c670
kunzaZa183 <yongvanichthee@gmail.com>
2023-10-16 13:14:37 +0700

    LIT and Codeforces E 24 Sep

--- main.cpp
+++ main.cpp
@@ -1,69 +1,152 @@
-// atcoder
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-const int MOD = 998244353, nums = 17;
-
-int allways[(1 << nums)] = {}, connected[(1 << nums)] = {};
-int fact[(1 << nums)];
-
-int logpow(int a, int b)
+const int maxn = 100;
+int arrpar[maxn], color[maxn], arrhead[maxn], arrdepth[maxn], mod[maxn];
+vector<int> adjlist[maxn];
+void init(int cur, int head, int depth)
 {
-  if (b == 0)
-    return 1;
-  if (b == 1)
-    return a;
-  int x = logpow(a, b / 2);
-  if (b % 2 == 0)
-    return x * x % MOD;
-  return x * x % MOD * a % MOD;
+    arrhead[cur] = head;
+    arrdepth[cur] = depth;
+    for (auto a : adjlist[cur])
+        if (a != arrpar[cur])
+            init(a, head, depth + 1);
 }
-
-signed main()
+void col3(int cur)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int n, m;
-  cin >> n >> m;
-  fact[0] = 1;
-  for (int i = 1; i <= (1 << nums); i++)
-    fact[i] = fact[i - 1] * i % MOD;
-  int arr[nums] = {}, arr2[nums] = {};
-  for (int i = 0; i < m; i++)
-  {
-    int x;
-    cin >> x;
-    arr[x - 1]++;
-  }
-  for (int i = 0; i < m; i++)
-  {
-    int x;
-    cin >> x;
-    arr2[x - 1]++;
-  }
-  int ways = 0;
-  for (int i = 1; i < (1 << n); i++)
-  {
-    vector<int> all;
-    for (int j = 0; j < n; j++)
-      if (i & (1 << j))
-        all.push_back(j);
-    int sum = 0, sum2 = 0;
-    for (auto a : all)
-      sum += arr[a], sum2 += arr2[a];
-    if (sum == sum2)
+    for (auto a : adjlist[cur])
+        if (a != arrpar[cur])
+        {
+            if (color[cur] == 1)
+                color[a] = 2;
+            else if (color[cur] == 2)
+                color[a] = 3;
+            else if (color[cur] == 3)
+                color[a] = 1;
+            col3(a);
+        }
+}
+void col2(int cur)
+{
+    for (auto a : adjlist[cur])
+        if (a != arrpar[cur])
+        {
+            if (color[cur] == 1)
+                color[a] = 2;
+            else if (color[cur] == 2)
+                color[a] = 1;
+            col2(a);
+        }
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    arrpar[0] = 0;
+    for (int i = 1; i < n; i++)
     {
-      allways[i] = fact[sum];
-      connected[i] = allways[i];
-      for (int j = i - 1; j >= 0; j--)
-      {
-        j &= i;
-        if (((1 << all.back()) & j) == 0)
-          break;
-        connected[i] = (connected[i] - connected[j] * allways[i ^ j] % MOD + MOD) % MOD;
-      }
-      ways = (ways + connected[i] * fact[m - sum] % MOD) % MOD;
+        cin >> arrpar[i];
+        arrpar[i]--;
+        adjlist[i].push_back(arrpar[i]), adjlist[arrpar[i]].push_back(i);
     }
-  }
-  cout << ways * logpow(fact[m], MOD - 2) % MOD << '\n';
+    int tmp;
+    for (int i = 1; i < n; i++)
+        if (adjlist[i].size() != 1)
+            goto A;
+    cout << 1 << endl;
+    for (int i = 1; i < n; i++)
+        cout << 1 << " ";
+    cout << endl;
+    while (cin >> tmp)
+    {
+        if (tmp != 0)
+            return 0;
+        int x;
+        cin >> x;
+        cout << 1 << endl;
+    }
+A:
+    for (int i = 1; i < n; i++)
+        mod[i] = -1;
+    for (auto a : adjlist[0])
+        init(a, a, 0);
+    bool three = 0;
+    for (int i = 1; i < n; i++)
+        if (adjlist[i].size() == 2)
+        {
+            if (mod[arrhead[i]] == -1)
+                mod[arrhead[i]] = arrdepth[i] % 2;
+            else if (mod[arrhead[i]] != arrdepth[i] % 2)
+                three = 1;
+        }
+    if (three)
+    {
+        cout << 3 << endl;
+        for (auto a : adjlist[0])
+        {
+            color[a] = 1;
+            col3(a);
+        }
+        for (int i = 1; i < n; i++)
+            cout << color[i] << " ";
+        cout << endl;
+        int num;
+        while (cin >> num)
+        {
+            if (num != 0)
+                return 0;
+            int arr[3];
+            for (int i = 0; i < 3; i++)
+                cin >> arr[i];
+            int sum = 0;
+            for (int i = 0; i < 3; i++)
+                sum += arr[i];
+            if (sum == 1)
+            {
+                for (int i = 0; i < 3; i++)
+                    if (arr[i] == 1)
+                    {
+                        cout << i + 1 << endl;
+                        break;
+                    }
+            }
+            else if (arr[0] == 0)
+                cout << 2 << endl;
+            else if (arr[1] == 0)
+                cout << 3 << endl;
+            else if (arr[2] == 0)
+                cout << 1 << endl;
+        }
+    }
+    else
+    {
+        cout << 2 << endl;
+        for (auto a : adjlist[0])
+            if (mod[a] <= 0)
+            {
+                color[a] = 1;
+                col2(a);
+            }
+            else if (mod[a] == 1)
+            {
+                color[a] = 2;
+                col2(a);
+            }
+        for (int i = 1; i < n; i++)
+            cout << color[i] << " ";
+        cout << endl;
+        int num;
+        while (cin >> num)
+        {
+            if (num != 0)
+                return 0;
+            int r, b;
+            cin >> r >> b;
+            if (r == 1)
+                cout << 1 << endl;
+            else if (b == 1)
+                cout << 2 << endl;
+        }
+    }
 }
