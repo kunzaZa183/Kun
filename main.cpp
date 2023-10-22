878b49879b110288a28f35b720c77e4d485fb83c
kunzaZa183 <yongvanichthee@gmail.com>
2023-10-22 15:44:14 +0700

    I hate coding

--- main.cpp
+++ main.cpp
@@ -1,102 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int fiveeight = 390625, base = 5, digits = 8;
-int dist[fiveeight];
-int xbit(int num, int x) // 0 based, right to left
-{
-	while (x--)
-		num /= base;
-	return num % base;
-}
-int gpow(int a, int b)
-{
-	int num = 1;
-	while (b--)
-		num *= a;
-	return num;
-}
+const int maxn = 500;
+int arr[maxn], dp[maxn][maxn] = {}, dp2[maxn][maxn] = {}; // val k
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	for (int i = 0; i < fiveeight; i++)
-		dist[i] = INT_MAX;
-	queue<pair<int, int>> qs; // state dist
-	for (int i = 0; i < (1 << digits); i++)
-	{
-		int num = 0;
-		for (int j = digits - 1; j >= 0; j--)
-		{
-			num *= base;
-			if ((1 << j) & i)
-			{
-				num += base - 1;
-				continue;
-			}
-			num += j / 2;
-		}
-		qs.emplace(num, 0);
-	}
-	while (!qs.empty())
-	{
-		pair<int, int> tmp = qs.front();
-		qs.pop();
-		if (dist[tmp.first] == INT_MAX)
-		{
-			dist[tmp.first] = tmp.second;
-			int nextnum1 = 0, nextnum2 = 0, nextnum3 = 0, nextnum4 = 0;
-			for (int j = digits - 1; j >= 0; j--)
-			{
-				nextnum1 *= base, nextnum2 *= base, nextnum3 *= base, nextnum4 *= base;
-				int val = xbit(tmp.first, j);
-				if (val == 4)
-				{
-					nextnum1 += 4, nextnum2 += 4, nextnum3 += 4, nextnum4 += 4;
-					continue;
-				}
-				int a = val >> 1, b = val % 2, m = j % 2;
-				nextnum1 += ((a & b) << 1) + b;
-				nextnum2 += ((a | b) << 1) + b;
-				nextnum3 += (a << 1) + a ^ b;
-				nextnum4 += (a << 1) + b ^ m;
-			}
-			qs.emplace(nextnum1, tmp.second + 1);
-			qs.emplace(nextnum2, tmp.second + 1);
-			qs.emplace(nextnum3, tmp.second + 1);
-			qs.emplace(nextnum4, tmp.second + 1);
-		}
-	}
-	int tests;
-	cin >> tests;
-	while (tests--)
-	{
-		int a, b, c, d, m;
-		cin >> a >> b >> c >> d >> m;
-		int num = 0;
-		for (int i = 0; i < digits; i++)
-		{
-			num *= base;
-			num += base - 1;
-		}
-		for (int i = 0; i < 30; i++)
-		{
-			int a2 = bool(a & (1 << i)), b2 = bool(b & (1 << i)), c2 = bool(c & (1 << i)), d2 = bool(d & (1 << i)), m2 = bool(m & (1 << i));
-			int val = xbit(num, (a2 << 2) + (b2 << 1) + m2);
-			if (val == 4)
-			{
-				num -= val * gpow(5, (a2 << 2) + (b2 << 1) + m2);
-				num += ((c2 << 1) + d2) * gpow(5, (a2 << 2) + (b2 << 1) + m2);
-			}
-			else if (val != ((c2 << 1) + d2))
-			{
-				cout << "-1\n";
-				goto A;
-			}
-		}
-		if (dist[num] == INT_MAX)
-			cout << "-1\n";
-		else
-			cout << dist[num] << "\n";
-	A:;
-	}
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        arr[i]--;
+    }
+    dp[arr[0]][0] = 1;
+    for (int i = 1; i < n; i++)
+    {
+        0.
+        +3895*/76
+    }
 }
--- main2.cpp
+++ main2.cpp
@@ -1,8 +1,99 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int maxn = 100000;
+pair<int, int> allsegments[maxn];
+struct event
+{
+    int point;
+    bool start;
+    int id;
+    event(int a, int b, int c)
+    {
+        point = a, start = b, id = c;
+    }
+    friend bool operator<(const event& a, const event& b)
+    {
+        return a.point < b.point;
+    }
+};
+vector<event> allpoints;
+int segtree[8 * maxn], lazy[8 * maxn];
+void lazyv(int curin)
+{
+    segtree[curin] += lazy[curin];
+    if (curin * 2 + 1 < 4 * maxn)
+        lazy[curin * 2 + 1] += lazy[curin];
+    if (curin * 2 + 2 < 4 * maxn)
+        lazy[curin * 2 + 2] += lazy[curin];
+    lazy[curin] = 0;
+}
+void update(int curin, int curl, int curr, int ql, int qr, int val)
+{
+    lazyv(curin);
+    if (ql > curr || qr < curl)
+        return;
+    if (ql <= curl && curr <= qr)
+    {
+        lazy[curin] = val;
+        lazyv(curin);
+        return;
+    }
+    update(curin * 2 + 1, curl, (curl + curr) / 2, ql, qr, val), update(curin * 2 + 2, (curl + curr) / 2 + 1, curr, ql, qr, val);
+    segtree[curin] = max(segtree[curin * 2 + 1], segtree[curin * 2 + 2]);
+}
+int lowerb(int num, vector<int>& vi)
+{
+    return lower_bound(vi.begin(), vi.end(), num) - vi.begin();
+}
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cout << bool(5) << "\n";
+    int tests;
+    cin >> tests;
+    while (tests--)
+    {
+        memset(segtree, 0, sizeof segtree);
+        memset(lazy, 0, sizeof lazy);
+        int n, m;
+        cin >> n >> m;
+        set<int> si;
+        si.insert(0);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> allsegments[i].first >> allsegments[i].second;
+            allsegments[i].first--, allsegments[i].second--;
+            si.insert(allsegments[i].first), si.insert(allsegments[i].second + 1);
+        }
+        allpoints.clear();
+        for (int i = 0; i < n; i++)
+            allpoints.emplace_back(allsegments[i].first, 1, i), allpoints.emplace_back(allsegments[i].second + 1, 0, i);
+        sort(allpoints.begin(), allpoints.end());
+        vector<int> usethis;
+        for (auto a : si)
+            usethis.push_back(a);
+        for (int i = 0; i < n; i++)
+            update(0, 0, usethis.size() - 1, lowerb(allsegments[i].first, usethis), lowerb(allsegments[i].second + 1, usethis) - 1, 1);
+        int curin = 0;
+        int maxi = 0;
+        for (auto a : usethis)
+        {
+            while (curin < allpoints.size())
+            {
+                if (allpoints[curin].point == a)
+                {
+                    if (allpoints[curin].start)
+                        update(0, 0, usethis.size() - 1, lowerb(allsegments[allpoints[curin].id].first, usethis), lowerb(allsegments[allpoints[curin].id].second + 1, usethis) - 1, -1);
+                    else
+                        update(0, 0, usethis.size() - 1, lowerb(allsegments[allpoints[curin].id].first, usethis), lowerb(allsegments[allpoints[curin].id].second + 1, usethis) - 1, 1);
+                    curin++;
+                }
+                else
+                    break;
+            }
+            if (a < m)
+                maxi = max(maxi, segtree[0]);
+        }
+        cout << maxi << '\n';
+    }
 }
