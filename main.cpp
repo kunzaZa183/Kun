656c68dce6569f34213d831ea154429424bf8f0e
kunzaZa183 <yongvanichthee@gmail.com>
2023-11-07 18:44:57 +0700

    1038

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 500;
-int arr[maxn], dp[maxn][maxn] = {} /* val k */, added[maxn];
+const int maxn = 20;
+double arr[maxn][maxn] = {};
+double dp[1 << maxn] = {};
 int main()
 {
     ios::sync_with_stdio(0);
@@ -9,30 +10,25 @@ int main()
     int n;
     cin >> n;
     for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-        arr[i]--;
-    }
-    dp[arr[0]][0] = 1;
-    for (int i = 1; i < n; i++)
-    {
-        memset(added, 0, sizeof added);
         for (int j = 0; j < n; j++)
-            for (int k = 0; k < n; k++)
-                if (arr[i] < arr[j])
-                    added[k + 1] += dp[arr[j]][k];
-                else
-                    added[k] += dp[arr[j]][k];
-        memset(dp, 0, sizeof dp);
-        for (int j = 0; j <= i; j++)
-            for (int k = 0; k < n; k++)
-                dp[arr[j]][k] += added[k];
-    }
-    memset(added, 0, sizeof added);
-    for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i][j];
+            arr[i][j] /= 100;
+        }
+    vector<vector<int>> cur(n + 1);
+    for (int i = 0; i < (1 << n); i++)
+    {
+        int ct = 0;
         for (int j = 0; j < n; j++)
-            added[i] += dp[j][i];
+            if (i & (1 << j))
+                ct++;
+        cur[ct].push_back(i);
+    }
+    dp[0] = 1;
     for (int i = 0; i < n; i++)
-        cout << added[i] << ' ';
-    cout << "\n";
+        for (auto a : cur[i])
+            for (int k = 0; k < n; k++)
+                if (((1 << k) & a) == 0)
+                    dp[(1 << k) | a] = max(dp[(1 << k) | a], dp[a] * arr[i][k]);
+    cout << fixed << setprecision(12) << dp[(1 << n) - 1] * 100 << '\n';
 }
--- main3.cpp
+++ main3.cpp
@@ -1,74 +1,33 @@
-//CF E2
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 200000;
-int arrend[maxn + 1];
-vector<int> nums[maxn + 1];
-int n, k;
-int find_ans(int in)
+double dist(pair<double, double> a, pair<double, double> b)
 {
-    if (in >= n)
-        return in;
-    if (nums[in].size() <= k)
-        return in;
-    arrend[in] = find_ans(arrend[in]);
-    return arrend[in];
+    return sqrt((b.first - a.first) * (b.first - a.first) + (b.second - a.second) * (b.second - a.second));
 }
-const int maxm = 200000;
-pair<int, int> alldays[maxm];
-const int maxk = 10;
-// int dp[maxk + 1][maxn + 1];//left, before number
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int tests;
-    cin >> tests;
-
-    while (tests--)
+    int n;
+    cin >> n;
+    vector<pair<double, double>> vpii(n);
+    for (auto &a : vpii)
     {
-        int m;
-        cin >> n >> m >> k;
-        for (int i = 0; i < n; i++)
-        {
-            nums[i].clear();
-            arrend[i] = i + 1;
-        }
-
-        for (int i = 0; i < m; i++)
-        {
-            cin >> alldays[i].first >> alldays[i].second;
-            alldays[i].first--, alldays[i].second--;
-            int l = alldays[i].first, r = alldays[i].second;
-            while (l <= r)
-            {
-                nums[l].push_back(i);
-                l = find_ans(l + 1);
-            }
-        }
-
-        int alwaysdry = 0;
-        map<vector<int>, int> pairmap;
-        map<int, int> mii;
-        for (int i = 0; i < n; i++)
-            if (nums[i].size() == 1)
-                mii[nums[i][0]]++;
-            else if (nums[i].size() == 2)
-                pairmap[nums[i]]++;
-            else if (nums[i].empty())
-                alwaysdry++;
-        vector<int> tmp;
-        for (auto a : mii)
-            tmp.push_back(a.second);
-        sort(tmp.begin(), tmp.end(), greater<int>());
-        int maxi = 0;
-        if (tmp.size() >= 2)
-            maxi = tmp[0] + tmp[1];
-        else if (tmp.size() == 1)
-            maxi = tmp[0];
-
-        for (auto a : pairmap)
-            maxi = max(maxi, a.second + mii[a.first[0]] + mii[a.first[1]]);
-        cout << maxi + alwaysdry << "\n";
+        string receive;
+        cin >> receive;
+        int index = receive.find(',');
+        a.first = stod(receive.substr(0, index));
+        a.second = stod(receive.substr(index + 1));
     }
+    vpii.emplace_back(0, 0);
+    sort(vpii.begin(), vpii.end());
+    double mini = INT_MAX;
+    do
+    {
+        double sum = dist(vpii.front(), vpii.back());
+        for (int i = 0; i < n; i++)
+            sum += dist(vpii[i + 1], vpii[i]);
+        mini = min(mini, sum);
+    } while (next_permutation(vpii.begin(), vpii.end()));
+    cout << fixed << setprecision(2) << mini << '\n';
 }
