37edab82cd9f592054bd9655157ad91fbf039cef
kunzaZa183 <yongvanichthee@gmail.com>
2023-12-03 22:00:09 +0700

    Close to finish collection
    Commented where stopped coding.
    Thinking abt time complexity

--- main.cpp
+++ main.cpp
@@ -1,112 +1,154 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-const int maxn = 200000;
-vector<int> adjlist[maxn], sccadjlist[maxn];
-set<int> tmpvsi[maxn];
-int arr[maxn], disc[maxn], link[maxn], whichscc[maxn], scclongest[maxn], sccminval[maxn];
-bitset<maxn> bsi;
-vector<int> si;
-int curtime = 0;
-vector<vector<int>> sccs;
-void dfs(int cur)
+
+const int maxn = 100000;
+int arr[maxn], parcent[maxn];
+vector<int> adjlist[maxn];
+bitset<maxn> used;
+
+int siz[maxn];
+int dfs1(int cur, int par)
 {
-    disc[cur] = curtime++;
-    link[cur] = disc[cur];
-    si.push_back(cur);
-    bsi[cur] = 1;
+    siz[cur] = 1;
     for (auto a : adjlist[cur])
-        if (disc[a] == -1)
-        {
-            dfs(a);
-            link[cur] = min(link[cur], link[a]);
-        }
-        else if (bsi[a])
-            link[cur] = min(link[cur], link[a]);
-    if (link[cur] == disc[cur])
-    {
-        sccs.push_back(vector<int>());
-        while (!si.empty())
-        {
-            int tmp = si.back();
-            si.pop_back();
-            bsi[tmp] = 0;
-            whichscc[tmp] = sccs.size() - 1;
-            sccs.back().push_back(tmp);
-            if (sccs.back().back() == cur)
-                break;
-        }
-    }
+        if (a != par && !used[a])
+            siz[cur] += dfs1(a, cur);
+    return siz[cur];
 }
-pair<int, int> sccdfs(int cur)
+
+int numintree;
+
+int dfs2(int cur, int par)
 {
-    if (scclongest[cur] == -1)
-    {
-        scclongest[cur] = sccs[cur].size();
-        sccminval[cur] = 0;
-        for (auto a : sccs[cur])
-            sccminval[cur] += arr[a];
-        pair<int, int> optimal(0, 0);
-        for (auto a : sccadjlist[cur])
-        {
-            pair<int, int> tmp = sccdfs(a);
-            if ((tmp.first > optimal.first) || (tmp.first == optimal.first && tmp.second < optimal.second))
-                optimal.swap(tmp);
-        }
-        scclongest[cur] += optimal.first;
-        sccminval[cur] += optimal.second;
-    }
-    return {scclongest[cur], sccminval[cur]};
+    for (auto a : adjlist[cur])
+        if (a != par && !used[a])
+            if (siz[a] * 2 > numintree)
+                return dfs2(a, par);
+    return cur;
 }
+
+int curcent;
+
+vector<int> segtree[maxn], lazy[maxn];
+void lazyv(int curin)
+{
+    segtree[curcent][curin] += lazy[curcent][curin];
+    if (curin * 2 + 1 < lazy[curcent].size())
+        lazy[curcent][curin * 2 + 1] += lazy[curcent][curin];
+    if (curin * 2 + 2 < lazy[curcent].size())
+        lazy[curcent][curin * 2 + 2] += lazy[curcent][curin];
+    lazy[curcent][curin] = 0;
+}
+
+int ql, qr, val;
+void update(int curin, int curl, int curr)
+{
+    lazyv(curin);
+    if (qr < curl || curr > ql)
+        return;
+    if (ql <= curl && curr <= qr)
+    {
+        lazy[curcent][curin] = val;
+        lazyv(curin);
+        return;
+    }
+    update(curin * 2 + 1, curl, (curl + curr) / 2), update(curin * 2 + 2, (curl + curr) / 2 + 1, curr);
+    segtree[curin] = max(segtree[curin * 2 + 1], segtree[curin * 2 + 2]);
+}
+
+int query(int curin, int curl, int curr)
+{
+    lazyv(curin);
+    if (qr < curl || curr > ql)
+        return 0;
+    if (ql <= curl && curr <= qr)
+        return segtree[curcent][curin];
+    return max(query(curin * 2 + 1, curl, (curl + curr) / 2), query(curin * 2 + 2, (curl + curr) / 2 + 1, curr));
+}
+
+int timer;
+map<int, int> starttime[maxn], endtime[maxn];
+int eulertour(int cur, int par, int curval)
+{
+    siz[cur] = 1;
+    ql = timer, qr = timer, val = curval;
+    update(0, 0, numintree - 1);
+    starttime[curcent][cur] = timer++;
+    for (auto a : adjlist[cur])
+        if (a != par && !used[a])
+            siz[cur] += eulertour(a, cur, curval + arr[a]);
+    endtime[curcent][cur] = timer;
+    return siz[cur];
+}
+
+int lastans;
+void eachcent(int cur, int siz, int oldcent)
+{
+    dfs1(cur, -1);
+
+    numintree = siz;
+    int cent = dfs2(cur, -1);
+
+    parcent[cent] = oldcent;
+
+    timer = 0;
+    curcent = cent;
+    segtree[cent].resize(4 * siz), lazy[cent].resize(4 * siz);
+    eulertour(cur, -1, 0);
+
+    int max1 = 0, max2 = 0;
+    for (auto a : adjlist[cent])
+        if (!used[a])
+        {
+            ql = starttime[cent][a], qr = endtime[cent][a] - 1;
+            int res = query(0, 0, siz - 1);
+            if (res >= max1)
+            {
+                swap(max1, max2);
+                max1 = res;
+            }
+            else if (res > max2)
+                max2 = res;
+        }
+    lastans = max(lastans, max1 + max2 + arr[cent]);
+
+    used[cent] = 1;
+    for (auto a : adjlist[cent])
+        if (!used[a])
+            eachcent(a, ::siz[a], cent);
+}
+
+
+
 signed main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int tests;
-    cin >> tests;
-    while (tests--)
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    lastans = arr[0];
+    for (int i = 0; i < n - 1; i++)
     {
-        int n, m;
-        cin >> n >> m;
-        for (int i = 0; i < n; i++)
-            adjlist[i].clear(), tmpvsi[i].clear();
-        for (int i = 0; i < n; i++)
-            cin >> arr[i];
-        for (int i = 0; i < m; i++)
+        int a, b;
+        cin >> a >> b;
+        adjlist[a - 1].push_back(b - 1), adjlist[b - 1].push_back(a - 1);
+    }
+    used.reset();
+    eachcent(0, n, -1);
+    cout<<lastans<<'\n';
+    while(m--)
+    {
+        int in,val;
+        cin>>in>>val;
+        in--;
+        int orig = in;
+        while(in != -1)
         {
-            int a, b;
-            cin >> a >> b;
-            a--, b--;
-            if (a != b)
-                tmpvsi[a].insert(b);
+            //deciding
+            in = parcent[in];
         }
-        for (int i = 0; i < n; i++)
-            for (auto a : tmpvsi[i])
-                adjlist[i].push_back(a);
-        memset(disc, -1, sizeof disc);
-        si.clear();
-        bsi.reset();
-        sccs.clear();
-        for (int i = 0; i < n; i++)
-            if (disc[i] == -1)
-                dfs(i);
-        for (int i = 0; i < n; i++)
-            tmpvsi[i].clear(), sccadjlist[i].clear();
-        for (int i = 0; i < n; i++)
-            for (auto a : adjlist[i])
-                tmpvsi[whichscc[i]].insert(whichscc[a]);
-        for (int i = 0; i < n; i++)
-            for (auto a : tmpvsi[i])
-                if (i != a)
-                    sccadjlist[i].push_back(a);
-        memset(scclongest, -1, sizeof scclongest), memset(sccminval, -1, sizeof sccminval);
-        pair<int, int> ans(0, 0);
-        for (int i = 0; i < sccs.size(); i++)
-        {
-            pair<int, int> tmp = sccdfs(i);
-            if ((tmp.first > ans.first) || (tmp.first == ans.first && tmp.second < ans.second))
-                ans.swap(tmp);
-        }
-        cout << ans.first << " " << ans.second << "\n";
     }
 }
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-def fact(num):
-    ans = 1
-    for i in range(1,num+1):
-        ans *= i
-    return ans
-print(fact(5))
-print(fact(100)/fact(85)/fact(15) * (0.11 ** 15) * (0.89 **85))
+string = input()
+total = 0
+allstring = ["one", "two" ,"three", "four", "five", "six", "seven", "eight","nine"]
+while string != "Q":
+    first = -1
+    last = 0
+    for i in range(len(string)):
+        if string[i].isdigit():
+            if first == -1:
+                first = int(string[i])
+            last = int(string[i])
+        else:
+            for num in allstring:
+                if len(string) - i >= len(num):
+                    if string[i:i + len(num)] == num:
+                        if first == -1:
+                            first = allstring.index(num) + 1
+                        last = allstring.index(num) + 1
+    total += first * 10 + last
+    string = input()
+print(total)
