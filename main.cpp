069efa357673eb9c2a27ca1264853b391c73d7c3
kunzaZa183 <yongvanichthee@gmail.com>
2023-12-04 21:43:31 +0700

    Collection done debug

--- main.cpp
+++ main.cpp
@@ -24,16 +24,17 @@ int dfs2(int cur, int par)
     for (auto a : adjlist[cur])
         if (a != par && !used[a])
             if (siz[a] * 2 > numintree)
-                return dfs2(a, par);
+                return dfs2(a, cur);
     return cur;
 }
 
 int curcent;
 
-vector<int> segtree[maxn], lazy[maxn];
+vector<pair<int, int>> segtree[maxn];
+vector<int> lazy[maxn];
 void lazyv(int curin)
 {
-    segtree[curcent][curin] += lazy[curcent][curin];
+    segtree[curcent][curin].first += lazy[curcent][curin], segtree[curcent][curin].second += lazy[curcent][curin];
     if (curin * 2 + 1 < lazy[curcent].size())
         lazy[curcent][curin * 2 + 1] += lazy[curcent][curin];
     if (curin * 2 + 2 < lazy[curcent].size())
@@ -45,26 +46,27 @@ int ql, qr, val;
 void update(int curin, int curl, int curr)
 {
     lazyv(curin);
-    if (qr < curl || curr > ql)
+
+    if (qr < curl || curr < ql)
         return;
     if (ql <= curl && curr <= qr)
     {
+        if (curl == curr)
+        {
+            segtree[curcent][curin].first += val;
+            segtree[curcent][curin].second = LLONG_MIN;
+            return;
+        }
         lazy[curcent][curin] = val;
         lazyv(curin);
         return;
     }
     update(curin * 2 + 1, curl, (curl + curr) / 2), update(curin * 2 + 2, (curl + curr) / 2 + 1, curr);
-    segtree[curin] = max(segtree[curin * 2 + 1], segtree[curin * 2 + 2]);
-}
 
-int query(int curin, int curl, int curr)
-{
-    lazyv(curin);
-    if (qr < curl || curr > ql)
-        return 0;
-    if (ql <= curl && curr <= qr)
-        return segtree[curcent][curin];
-    return max(query(curin * 2 + 1, curl, (curl + curr) / 2), query(curin * 2 + 2, (curl + curr) / 2 + 1, curr));
+    pair<int, int> pii1 = segtree[curcent][curin * 2 + 1], pii2 = segtree[curcent][curin * 2 + 2];
+    int arr[4] = { pii1.first, pii1.second, pii2.first, pii2.second };
+    sort(arr, arr + 4);
+    segtree[curcent][curin] = { arr[2], arr[3] };
 }
 
 int timer;
@@ -82,9 +84,19 @@ int eulertour(int cur, int par, int curval)
     return siz[cur];
 }
 
-int lastans;
+multiset<int> allans;
+int oldans[maxn];
+
 void eachcent(int cur, int siz, int oldcent)
 {
+    if (siz == 1)
+    {
+        parcent[cur] = oldcent;
+        oldans[cur] = arr[cur];
+        allans.insert(arr[cur]);
+        used[cur] = 1;
+        return;
+    }
     dfs1(cur, -1);
 
     numintree = siz;
@@ -95,31 +107,17 @@ void eachcent(int cur, int siz, int oldcent)
     timer = 0;
     curcent = cent;
     segtree[cent].resize(4 * siz), lazy[cent].resize(4 * siz);
-    eulertour(cur, -1, 0);
+    eulertour(cent, -1, 0);
 
-    int max1 = 0, max2 = 0;
-    for (auto a : adjlist[cent])
-        if (!used[a])
-        {
-            ql = starttime[cent][a], qr = endtime[cent][a] - 1;
-            int res = query(0, 0, siz - 1);
-            if (res >= max1)
-            {
-                swap(max1, max2);
-                max1 = res;
-            }
-            else if (res > max2)
-                max2 = res;
-        }
-    lastans = max(lastans, max1 + max2 + arr[cent]);
+    int ql = 0, qr = siz - 1;
+    oldans[cent] = segtree[cent][0].first + segtree[cent][0].second + arr[cent];
+    allans.insert(oldans[cent]);
 
     used[cent] = 1;
     for (auto a : adjlist[cent])
         if (!used[a])
             eachcent(a, ::siz[a], cent);
 }
-
-
 
 signed main()
 {
@@ -129,7 +127,6 @@ signed main()
     cin >> n >> m;
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    lastans = arr[0];
     for (int i = 0; i < n - 1; i++)
     {
         int a, b;
@@ -138,17 +135,38 @@ signed main()
     }
     used.reset();
     eachcent(0, n, -1);
-    cout<<lastans<<'\n';
-    while(m--)
+    cout << *allans.rbegin() << '\n';
+    while (m--)
     {
-        int in,val;
-        cin>>in>>val;
+        int in, val;
+        cin >> in >> val;
         in--;
-        int orig = in;
-        while(in != -1)
+        int change = val - arr[in];
+        arr[in] = val;
+
+        if (segtree[in].empty())
         {
-            //deciding
+            allans.erase(allans.find(oldans[in]));
+            oldans[in] = val;
+            allans.insert(val);
+        }
+        else
+        {
+            allans.erase(allans.find(oldans[in]));
+            oldans[in] = segtree[in][0].first + segtree[in][0].second + arr[in];
+            allans.insert(oldans[in]);
+        }
+        int orig = in;
+        in = parcent[in];
+        while (in != -1)
+        {
+            allans.erase(allans.find(oldans[in]));
+            curcent = in, ql = starttime[in][orig], qr = endtime[in][orig] - 1, ::val = change;
+            update(0, 0, segtree[curcent].size() / 4 - 1);
+            oldans[in] = segtree[in][0].first + segtree[in][0].second + arr[in];
+            allans.insert(oldans[in]);
             in = parcent[in];
         }
+        cout << *allans.rbegin() << '\n';
     }
 }
