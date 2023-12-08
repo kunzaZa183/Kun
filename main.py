c47c1cbc3359c79abf3fd1ac19997e9af0e7aa60
kunzaZa183 <yongvanichthee@gmail.com>
2023-12-08 23:03:31 +0700

    Advent of Code

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 string = input()
-total = 0
-allstring = ["one", "two" ,"three", "four", "five", "six", "seven", "eight","nine"]
+board = []
 while string != "Q":
-    first = -1
-    last = 0
-    for i in range(len(string)):
-        if string[i].isdigit():
-            if first == -1:
-                first = int(string[i])
-            last = int(string[i])
-        else:
-            for num in allstring:
-                if len(string) - i >= len(num):
-                    if string[i:i + len(num)] == num:
-                        if first == -1:
-                            first = allstring.index(num) + 1
-                        last = allstring.index(num) + 1
-    total += first * 10 + last
+    board.append(string)
     string = input()
-print(total)
+group = [[-1 for j in range(len(board[0]))] for i in range(len(board))]
+for i in range(len(board)):
+    
--- main2.cpp
+++ main2.cpp
@@ -1,136 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-const int maxn = 100000;
-int arr[maxn], parcent[maxn];
-vector<int> adjlist[maxn];
-bitset<maxn> used;
-
-int siz[maxn];
-int dfs1(int cur, int par)
+const int dx2[8] = {-1, -1, 0, 1, 1, 1, 0, -1}, dy2[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+int ct = 0;
+vector<string> vs;
+vector<vector<int>> group;
+int main()
 {
-	siz[cur] = 1;
-	for (auto a : adjlist[cur])
-		if (a != par && !used[a])
-			siz[cur] += dfs1(a, cur);
-	return siz[cur];
-}
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string s;
+    cin >> s;
+    while (s != "Q")
+    {
+        vs.push_back(s);
+        cin >> s;
+    }
+    int rows = vs.size(), cols = vs[0].size();
+    group.resize(rows, vector<int>(cols, -1));
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            if (isdigit(vs[i][j]))
+            {
+                if (j > 0)
+                    if (isdigit(vs[i][j - 1]))
+                    {
+                        group[i][j] = group[i][j - 1];
+                        continue;
+                    }
+                group[i][j] = ct++;
+            }
+    vector<int> val(ct, 0);
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            if (group[i][j] != -1)
+            {
+                val[group[i][j]] *= 10;
+                val[group[i][j]] += vs[i][j] - '0';
+            }
+    int total = 0;
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            if (vs[i][j] == '*')
+            {
+                set<int> si;
+                for (int k = 0; k < 8; k++)
+                    if (group[i + dx2[k]][j + dy2[k]] != -1)
+                        si.insert(group[i + dx2[k]][j + dy2[k]]);
+                if (si.size() == 2)
+                    total += val[*si.begin()] * val[*si.rbegin()];
+            }
 
-int numintree;
-
-int dfs2(int cur, int par)
-{
-	for (auto a : adjlist[cur])
-		if (a != par && !used[a])
-			if (siz[a] * 2 > numintree)
-				return dfs2(a, cur);
-	return cur;
-}
-
-int curcent;
-
-vector<int> segtree[maxn], lazy[maxn];
-void lazyv(int curin)
-{
-	segtree[curcent][curin] += lazy[curcent][curin];
-	if (curin * 2 + 1 < lazy[curcent].size())
-		lazy[curcent][curin * 2 + 1] += lazy[curcent][curin];
-	if (curin * 2 + 2 < lazy[curcent].size())
-		lazy[curcent][curin * 2 + 2] += lazy[curcent][curin];
-	lazy[curcent][curin] = 0;
-}
-
-int ql, qr, val;
-void update(int curin, int curl, int curr)
-{
-	lazyv(curin);
-
-	if (qr < curl || curr < ql)
-		return;
-	if (ql <= curl && curr <= qr)
-	{
-		lazy[curcent][curin] = val;
-		lazyv(curin);
-		return;
-	}
-	update(curin * 2 + 1, curl, (curl + curr) / 2), update(curin * 2 + 2, (curl + curr) / 2 + 1, curr);
-	segtree[curcent][curin] = max(segtree[curcent][curin * 2 + 1], segtree[curcent][curin * 2 + 2]);
-}
-
-int query(int curin, int curl, int curr)
-{
-	lazyv(curin);
-
-	if (qr < curl || curr < ql)
-		return 0;
-	if (ql <= curl && curr <= qr)
-		return segtree[curcent][curin];
-	return max(query(curin * 2 + 1, curl, (curl + curr) / 2), query(curin * 2 + 2, (curl + curr) / 2 + 1, curr));
-}
-
-int timer;
-map<int, int> starttime[maxn], endtime[maxn];
-int eulertour(int cur, int par, int curval)
-{
-	siz[cur] = 1;
-	ql = timer, qr = timer, val = curval;
-	update(0, 0, numintree - 1);
-	starttime[curcent][cur] = timer++;
-	for (auto a : adjlist[cur])
-		if (a != par && !used[a])
-			siz[cur] += eulertour(a, cur, curval + arr[a]);
-	endtime[curcent][cur] = timer;
-	return siz[cur];
-}
-
-map<int, int> parnextroot[maxn];
-
-int curnextroot;
-void dfs3(int cur, int par)
-{
-	parnextroot[curcent][cur] = curnextroot;
-	for (auto a : adjlist[cur])
-		if (!used[a] && a != par)
-			dfs3(a, cur);
-}
-
-multiset<int> allans, alldowncent[maxn];
-int oldans[maxn];
-
-int sumfirsttwo(multiset<int>& msi)
-{
-	auto it = msi.rbegin(), it2 = msi.rbegin();
-	it2++;
-	return *it + *it2;
-}
-
-void eachcent(int cur, int siz, int oldcent)
-{
-	dfs1(cur, -1);
-
-	numintree = siz;
-	int cent = dfs2(cur, -1);
-
-	parcent[cent] = oldcent;
-
-	timer = 0;
-	curcent = cent;
-	segtree[cent].resize(4 * siz), lazy[cent].resize(4 * siz);
-	eulertour(cent, -1, 0);
-
-	alldowncent[cent].insert(0), alldowncent[cent].insert(0);
-	for (auto a : adjlist[cent])
-	{
-		ql = starttime[cent][a], qr = endtime[cent][a] - 1;
-		alldowncent[cent].insert(query(0, 0, siz - 1));
-		curnextroot = a;
-		dfs3(a, cent);
-	}
-	oldans[cent] = sumfirsttwo(alldowncent[cent]) + arr[cent];
-	allans.insert(oldans[cent]);
-
-	used[cent] = 1;
-	for (auto a : adjlist[cent])
-		if (!used[a])
-			eachcent(a, ::siz[a], cent);
+    cout << total << '\n';
 }
