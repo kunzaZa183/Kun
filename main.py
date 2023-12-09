705727f9742e54e984368a3fdf5a20d483d6a0dc
kunzaZa183 <yongvanichthee@gmail.com>
2023-12-09 20:46:04 +0700

    Fun atcoder and lil of advent of code

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 string = input()
-board = []
+total = 0
+matches = []
 while string != "Q":
-    board.append(string)
+    string = string.split()
+    string.pop(0)
+    string.pop(0)
+    linein = 0
+    for i in range(len(string)):
+        if string[i] == "|":
+            linein = i
+            break
+    arr1 = []
+    arr2 = []
+    for i in range(linein):
+        arr1.append(string[i])
+    for i in range(linein + 1, len(string)):
+        arr2.append(string[i])
+    ct = 0
+    for i in arr1:
+        if i in arr2:
+            ct += 1
+    matches.append(ct)
     string = input()
-group = [[-1 for j in range(len(board[0]))] for i in range(len(board))]
-for i in range(len(board)):
-    
+frequency = [1 for i in range(len(matches))]
+for i in range(len(frequency)):
+    for j in range(i + 1, min(len(frequency), i + matches[i] + 1)):
+        frequency[j] += frequency[i]
+print(sum(frequency))
--- main2.cpp
+++ main2.cpp
@@ -1,54 +1,92 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int dx2[8] = {-1, -1, 0, 1, 1, 1, 0, -1}, dy2[8] = {0, 1, 1, 1, 0, -1, -1, -1};
-int ct = 0;
-vector<string> vs;
-vector<vector<int>> group;
+const int maxn = 5000, MOD = 998244353;
+queue<int> alldp[maxn + 1];
+int arr[maxn], sum[maxn + 1], nothing[maxn + 1];
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    string s;
-    cin >> s;
-    while (s != "Q")
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    if (arr[0] == -1)
     {
-        vs.push_back(s);
-        cin >> s;
+        for (int i = 1; i <= n; i++)
+            for (int j = 0; j < i - 1; j++)
+                alldp[i].push(0);
+        for (int i = 1; i <= n; i++)
+        {
+            alldp[i].push(1);
+            sum[i] = 1;
+        }
     }
-    int rows = vs.size(), cols = vs[0].size();
-    group.resize(rows, vector<int>(cols, -1));
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < cols; j++)
-            if (isdigit(vs[i][j]))
+    else
+    {
+        for (int i = 1; i <= n; i++)
+            for (int j = 0; j < i; j++)
+                alldp[i].push(0);
+        alldp[arr[0]].pop();
+        alldp[arr[0]].push(1);
+        sum[arr[0]] = 1;
+    }
+    for (int i = 1; i < n; i++)
+        if (arr[i] == -1)
+        {
+            int sumall = 0;
+            for (int j = 1; j <= n; j++)
+                sumall = (sumall + sum[j]) % MOD;
+            for (int j = 1; j <= n; j++)
             {
-                if (j > 0)
-                    if (isdigit(vs[i][j - 1]))
-                    {
-                        group[i][j] = group[i][j - 1];
-                        continue;
-                    }
-                group[i][j] = ct++;
+                int val = sumall - sum[j];
+                if (val < 0)
+                    val += MOD;
+                alldp[j].push(val);
+                sum[j] = (sum[j] + val) % MOD;
+                if (nothing[j])
+                {
+                    nothing[j]--;
+                    alldp[j].pop();
+                }
+                else
+                {
+                    sum[j] -= alldp[j].front();
+                    alldp[j].pop();
+                    if (sum[j] < 0)
+                        sum[j] += MOD;
+                }
             }
-    vector<int> val(ct, 0);
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < cols; j++)
-            if (group[i][j] != -1)
+        }
+        else
+        {
+            int sumall = 0;
+            for (int j = 1; j <= n; j++)
+                if (arr[i] != j)
+                    sumall = (sumall + sum[j]) % MOD;
+            alldp[arr[i]].push(sumall);
+            sum[arr[i]] = (sum[arr[i]] + sumall) % MOD;
+            if (nothing[arr[i]])
             {
-                val[group[i][j]] *= 10;
-                val[group[i][j]] += vs[i][j] - '0';
+                nothing[arr[i]]--;
+                alldp[arr[i]].pop();
             }
-    int total = 0;
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < cols; j++)
-            if (vs[i][j] == '*')
+            else
             {
-                set<int> si;
-                for (int k = 0; k < 8; k++)
-                    if (group[i + dx2[k]][j + dy2[k]] != -1)
-                        si.insert(group[i + dx2[k]][j + dy2[k]]);
-                if (si.size() == 2)
-                    total += val[*si.begin()] * val[*si.rbegin()];
+                sum[arr[i]] -= alldp[arr[i]].front();
+                alldp[arr[i]].pop();
+                if (sum[arr[i]] < 0)
+                    sum[arr[i]] += MOD;
             }
-
-    cout << total << '\n';
+            for (int j = 1; j <= n; j++)
+                if (arr[i] != j)
+                {
+                    nothing[j] = j;
+                    sum[j] = 0;
+                }
+        }
+    int all = 0;
+    for (int i = 1; i <= n; i++)
+        all = (all + sum[i]) % MOD;
+    cout << all << "\n";
 }
