68b58cc51d323c95e0b2171f9e5fb15b9be7a4e6
kunzaZa183 <yongvanichthee@gmail.com>
2023-12-22 11:08:00 +0700

    AoC Day 8

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-signed main()
+map<string, pair<string, string>> mspss;
+int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int time, dist;
-    cin >> time >> dist;
-    int ct = 0;
-    for (int i = 0; i < time; i++)
-        if (i * (time - i) > dist)
+    string order;
+    cin >> order;
+    string name1, equal, first, last;
+    while (cin >> name1 >> equal >> first >> last, name1 != "-1")
+        mspss[name1] = { first.substr(1, 3), last.substr(0, 3) };
+    vector<string> cur;
+    for (auto a : mspss)
+        if (a.first.back() == 'A')
+            cur.push_back(a.first);
+    vector<int> vi;
+    for (auto a : cur)
+    {
+        int in = 0, ct = 0;
+        while (a.back() != 'Z')
+        {
+            if (in == order.size())
+                in = 0;
+            if (order[in] == 'L')
+                a = mspss[a].first;
+            else if (order[in] == 'R')
+                a = mspss[a].second;
+            in++;
             ct++;
-    cout << ct << "\n";
+        }
+        vi.push_back(ct);
+    }
+    for (auto a : vi)
+        cout << a << '\n';
 }
