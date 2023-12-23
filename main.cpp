fa598840a5eb438a175ac813b2b33712b49e3412
kunzaZa183 <yongvanichthee@gmail.com>
2023-12-23 13:24:57 +0700

    95 on trainto

--- main.cpp
+++ main.cpp
@@ -1,61 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<vector<bool>> vvb;
-vector<string> vs;
-void recur(int curx, int cury)
-{
-    if (vvb[curx][cury])
-        return;
-    vvb[curx][cury] = 1;
-    char c = vs[curx][cury];
-    if (c == 'S' || c == '-')
-        recur(curx, cury + 1), recur(curx, cury - 1);
-    else if (c == '|')
-        recur(curx - 1, cury), recur(curx + 1, cury);
-    else if (c == 'L')
-        recur(curx - 1, cury), recur(curx, cury + 1);
-    else if (c == 'J')
-        recur(curx - 1, cury), recur(curx, cury - 1);
-    else if (c == '7')
-        recur(curx, cury - 1), recur(curx + 1, cury);
-    else if (c == 'F')
-        recur(curx, cury + 1), recur(curx + 1, cury);
-}
-
+const int maxn = 350, BIGNUM = 100000;
+int arr[maxn + 1], qsum[maxn + 1], dp[maxn + 1][maxn + 1][maxn + 1];
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    string s;
-    while (cin >> s, s != "Q")
-        vs.push_back(s);
-    vvb.resize(vs.size(), vector<bool>(vs.front().size(), 0));
-    for (int i = 0; i < vs.size(); i++)
-        for (int j = 0; j < vs[i].size(); j++)
-            if (vs[i][j] == 'S')
-            {
-                vs[i][j] == '-';
-                recur(i, j);
-            }
-    int sum = 0;
-    for (int i = 0; i < vs.size(); i++)
-        for (int j = 0; j < vs[i].size(); j++)
-            if (!vvb[i][j])
-            {
-                int tmpi = i, tmpj = j;
-                int ct = 0;
-                while (tmpi >= 0 && tmpj >= 0)
-                {
-                    if (vvb[tmpi][tmpj])
-                    {
-                        if (vs[tmpi][tmpj] == 'L' || vs[tmpi][tmpj] == '7')
-                            ct += 2;
-                        else
-                            ct++;
-                    }
-                    tmpi--, tmpj--;
-                }
-                sum += ct % 2;
-            }
-    cout << sum << '\n';
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n, actualk;
+	cin >> n >> actualk;
+	for (int i = 1; i <= n; i++)
+		cin >> arr[i];
+	sort(arr + 1, arr + 1 + n, greater<int>());
+	for (int i = 1; i <= n; i++)
+		qsum[i] += qsum[i - 1] + arr[i];
+	for (int i = 1; i <= maxn; i++)
+		for (int j = 1; j <= maxn; j++)
+			for (int k = 1; k <= maxn; k++)
+				dp[i][j][k] = BIGNUM;
+	for (int i = 1; i <= maxn; i++)
+		for (int j = i + 1; j <= maxn; j++)
+			for (int numleft = 0; numleft <= (j - i + 1); numleft++)
+			{
+				int numright = j - i + 1 - numleft;
+				dp[1][i][j] = min(dp[1][i][j], (qsum[i - 1 + numleft] - qsum[i - 1]) * (numleft - 1) + (qsum[j] - qsum[i + numleft - 1]) * (numright - 1) + 2 * numleft * numright);
+			}
+	for (int k = 2; k <= actualk; k++)
+		for (int i = 1; i <= n; i++)
+			for (int j = i + 2 * k - 1; j <= n; j++)
+				for (int x = i; x < n / k + i + 1 && x < j; x++)
+					for (int y = x + 1; y <= j; y++)
+					{
+						dp[k][i][j] = min(dp[k][i][j], dp[k - 1][x + 1][y - 1] + (x - i) * (qsum[x] - qsum[i - 1]) + (j - y) * (qsum[j] - qsum[y - 1]) + 2 * (x - i + 1) * (j - y + 1));
+					}
+	cout << dp[actualk][1][n] << "\n";
 }
