ae5c0a973a7baca8933029e00606c918216dedbd
kunzaZa183 <yongvanichthee@gmail.com>
2023-12-25 13:32:07 +0700

    Aoc Day 11

--- main2.cpp
+++ main2.cpp
@@ -1,63 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-const int maxn = 200000;
-int arr[maxn];
 signed main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int tests;
-    cin >> tests;
-    while (tests--)
+    vector<string> vs;
+    string s;
+    while (cin >> s, s != "Q")
+        vs.push_back(s);
+    vector<bool> row(vs.size()), column(vs.front().size());
+    for (int i = 0; i < vs.size(); i++)
+        if (find(vs[i].begin(), vs[i].end(), '#') == vs[i].end())
+            row[i] = 1;
+    for (int j = 0; j < vs[0].size(); j++)
     {
-        int n, k;
-        cin >> n >> k;
-        for (int i = 0; i < n; i++)
-            cin >> arr[i];
-        for (int i = 0; i < n; i++)
-            if (arr[i] != arr[0])
+        for (int i = 0; i < vs.size(); i++)
+            if (vs[i][j] == '#')
                 goto A;
-        cout << "0\n";
-        continue;
-    A:
-        for (int i = 0; i < n; i++)
-            if (arr[i] == k)
-            {
-                cout << "-1\n";
-                goto B;
-            }
-        for (int i = 0; i < n; i++)
-            if (arr[0] > k != arr[i] > k)
-            {
-                cout << "-1\n";
-                goto B;
-            }
-        goto C;
-    B:
-        continue;
-    C:
-        if (arr[0] > k)
-        {
-            int firstgcd = arr[0] - k;
-            for (int i = 1; i < n; i++)
-                firstgcd = gcd(firstgcd, arr[i] - k);
-            firstgcd += k;
-            int sum = 0;
-            for (int i = 0; i < n; i++)
-                sum += (arr[i] - firstgcd) / (firstgcd - k);
-            cout << sum << '\n';
-        }
-        else if (arr[0] < k)
-        {
-            int firstgcd = k - arr[0];
-            for (int i = 1; i < n; i++)
-                firstgcd = gcd(firstgcd, k-arr[i]);
-            firstgcd = k - firstgcd;
-            int sum = 0;
-            for (int i = 0; i < n; i++)
-                sum += (firstgcd - arr[i]) / (k - firstgcd);
-            cout << sum << '\n';
-        }
+        column[j] = 1;
+    A:;
     }
+    vector<pair<int, int>> vpii;
+    for (int i = 0; i < vs.size(); i++)
+        for (int j = 0; j < vs[0].size(); j++)
+            if (vs[i][j] == '#')
+                vpii.emplace_back(i, j);
+    int sum = 0;
+    for (int i = 0; i < vpii.size(); i++)
+        for (int j = i + 1; j < vpii.size(); j++)
+        {
+            pair<int, int> x1 = {vpii[i].first, vpii[j].first}, y1 = {vpii[i].second, vpii[j].second};
+            if (x1.first > x1.second)
+                swap(x1.first, x1.second);
+            if (y1.first > y1.second)
+                swap(y1.first, y1.second);
+            for (int k = x1.first; k < x1.second; k++)
+                if (row[k])
+                    sum += 999999;
+            for (int k = y1.first; k < y1.second; k++)
+                if (column[k])
+                    sum += 999999;
+            sum += (x1.second - x1.first) + (y1.second - y1.first);
+        }
+    cout << sum << '\n';
 }
