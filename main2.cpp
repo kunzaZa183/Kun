95132a71358de1b7b48a572c1f2d434cacdd84a7
kunzaZa183 <yongvanichthee@gmail.com>
2023-12-27 21:25:42 +0700

    Meet in the middle: CSES

--- main2.cpp
+++ main2.cpp
@@ -1,47 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+vector<int> vi, vi2;
 signed main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    vector<string> vs;
-    string s;
-    while (cin >> s, s != "Q")
-        vs.push_back(s);
-    vector<bool> row(vs.size()), column(vs.front().size());
-    for (int i = 0; i < vs.size(); i++)
-        if (find(vs[i].begin(), vs[i].end(), '#') == vs[i].end())
-            row[i] = 1;
-    for (int j = 0; j < vs[0].size(); j++)
+    int num, x;
+    cin >> num >> x;
+    vector<int> arr(num);
+    for (auto& a : arr)
+        cin >> a;
+    int f = num / 2;
+    int l = num - f;
+    for (int i = 0; i < (1 << f); i++)
     {
-        for (int i = 0; i < vs.size(); i++)
-            if (vs[i][j] == '#')
-                goto A;
-        column[j] = 1;
+        int sum = 0;
+        for (int j = 0; j < f; j++)
+            if (i & (1 << j))
+            {
+                sum += arr[j];
+                if (sum > x)
+                    goto B;
+            }
+        vi.push_back(sum);
+    B:;
+    }
+    for (int i = 0; i < (1 << l); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < l; j++)
+            if (i & (1 << j))
+            {
+                sum += arr[f + j];
+                if (sum > x)
+                    goto A;
+            }
+        vi2.push_back(sum);
     A:;
     }
-    vector<pair<int, int>> vpii;
-    for (int i = 0; i < vs.size(); i++)
-        for (int j = 0; j < vs[0].size(); j++)
-            if (vs[i][j] == '#')
-                vpii.emplace_back(i, j);
-    int sum = 0;
-    for (int i = 0; i < vpii.size(); i++)
-        for (int j = i + 1; j < vpii.size(); j++)
+    sort(vi.begin(), vi.end());
+    sort(vi2.begin(), vi2.end(), greater<int>());
+    vector<pair<int, int>> vpii1, vpii2;
+    for (auto a : vi)
+        if (vpii1.empty())
+            vpii1.emplace_back(a, 1);
+        else if (vpii1.back().first == a)
+            vpii1.back().second++;
+        else
+            vpii1.emplace_back(a, 1);
+
+    for (auto a : vi2)
+        if (vpii2.empty())
+            vpii2.emplace_back(a, 1);
+        else if (vpii2.back().first == a)
+            vpii2.back().second++;
+        else
+            vpii2.emplace_back(a, 1);
+
+    int pt1 = 0, pt2 = 0;
+    int res = 0;
+    while (pt1 < vpii1.size() && pt2 < vpii2.size())
+    {
+        if (vpii1[pt1].first + vpii2[pt2].first == x)
         {
-            pair<int, int> x1 = {vpii[i].first, vpii[j].first}, y1 = {vpii[i].second, vpii[j].second};
-            if (x1.first > x1.second)
-                swap(x1.first, x1.second);
-            if (y1.first > y1.second)
-                swap(y1.first, y1.second);
-            for (int k = x1.first; k < x1.second; k++)
-                if (row[k])
-                    sum += 999999;
-            for (int k = y1.first; k < y1.second; k++)
-                if (column[k])
-                    sum += 999999;
-            sum += (x1.second - x1.first) + (y1.second - y1.first);
+            res += vpii1[pt1].second * vpii2[pt2].second;
+            pt2++;
         }
-    cout << sum << '\n';
+        else if (vpii1[pt1].first + vpii2[pt2].first > x)
+            pt2++;
+        else
+            pt1++;
+    }
+    cout << res << "\n";
 }
