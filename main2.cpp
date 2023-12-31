21586f9bc8413ae4336d5e74345942d25dc732d7
kunzaZa183 <yongvanichthee@gmail.com>
2023-12-31 16:54:35 +0700

    More Cses

--- main2.cpp
+++ main2.cpp
@@ -1,22 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-const int maxn = 200000;
-int arr[maxn];
+const int dx[8] = {-2, -2, -1, 1, 2, 2, 1, -1}, dy[8] = {-1, 1, 2, 2, 1, -1, -2, -2};
+bool valid(int x, int y, int n)
+{
+    return x >= 0 && x < n && y >= 0 && y < n;
+}
 signed main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int num;
     cin >> num;
-    for (int i = 0; i < num; i++)
-        cin >> arr[i];
-    int moves = 0;
-    for (int i = 1; i < num; i++)
-        if (arr[i] < arr[i - 1])
+    for (int i = 1; i <= num; i++)
+    {
+        int all = i * i * (i * i - 1) / 2;
+        int hit = 0;
+        if (i <= 4)
         {
-            moves += arr[i - 1] - arr[i];
-            arr[i] = arr[i - 1];
+            for (int j = 0; j < i; j++)
+                for (int k = 0; k < i; k++)
+                    for (int l = 0; l < 8; l++)
+                        if (valid(dx[l] + j, dy[l] + k, i))
+                            hit++;
+            hit /= 2;
         }
-    cout << moves << "\n";
+        else
+        {
+            hit = (i - 4) * (i - 4) * 8;
+            hit += (4 * 2);
+            hit += (8 * 3);
+            hit += (i - 3) * 4 * 6;
+            hit += ((i * i) - (i - 4) * (i - 4) - 4 - 8 - (i - 3) * 4) * 6;
+            hit -= 8 * i;
+            hit /= 2;
+            hit += 12;
+        }
+        all -= hit;
+        cout << all << "\n";
+    }
 }
--- main4.cpp
+++ main4.cpp
@@ -1,28 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+#define int long long
+const int dx[8] = {-2, -2, -1, 1, 2, 2, 1, -1}, dy[8] = {-1, 1, 2, 2, 1, -1, -2, -2};
+bool valid(int x, int y, int n)
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    freopen("in.txt", "w", stdout);
-    srand(time(NULL));
-    int n = 10, k = 5 + rand() % (n - 5);
-    vector<int> allnum;
-    for (int i = 1; i <= n; i++)
-        allnum.push_back(i);
-    vector<int> allnum2 = allnum;
-    vector<int> vi, vi2;
-    for (int i = 0; i < k; i++)
-    {
-        int x = rand() % allnum.size(), y = rand() % allnum2.size();
-        vi.push_back(allnum[x]), vi2.push_back(allnum2[y]);
-        allnum.erase(allnum.begin() + x), allnum2.erase(allnum2.begin() + y);
-    }
-    cout << n << ' ' << k << '\n';
-    for (auto a : vi)
-        cout << a << ' ';
-    cout << '\n';
-    for (auto a : vi2)
-        cout << a << ' ';
-    cout << "\n";
+    return x >= 0 && x < n && y >= 0 && y < n;
+}
+signed main()
+{
+    freopen("out.txt", "w", stdout);
+    cout << "2\n3\n";
 }
