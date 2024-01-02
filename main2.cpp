397394dbcf5b86af6c69f169c13290bae39218ec
kunzaZa183 <yongvanichthee@gmail.com>
2024-01-02 21:29:21 +0700

    IDK Grid Paths

--- main2.cpp
+++ main2.cpp
@@ -1,24 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = (1 << 16);
-bitset<maxn> bs;
-void move(int length, int cur, int to, int canplace)
-{
-    if (length == 1)
-    {
-        cout << cur << " " << to << "\n";
-        return;
-    }
-    move(length - 1, cur, canplace, to);
-    cout << cur << " " << to << '\n';
-    move(length - 1, canplace, to, cur);
-}
+const int maxn = 7;
+int arr[maxn][maxn], second[maxn][maxn];
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n;
-    cin >> n;
-    cout << (1 << n) - 1 << "\n";
-    move(n, 1, 3, 2);
+    string s;
+    cin >> s;
+    arr[0][0] = 1;
+    for (auto a : s)
+    {
+        if (a == '?')
+        {
+            for (int i = 0; i < maxn; i++)
+                for (int j = 0; j < maxn - 1; j++)
+                    second[i][j + 1] += arr[i][j];
+            for (int i = 0; i < maxn - 1; i++)
+                for (int j = 0; j < maxn; j++)
+                    second[i + 1][j] += arr[i][j];
+            for (int i = 1; i < maxn; i++)
+                for (int j = 0; j < maxn; j++)
+                    second[i - 1][j] += arr[i][j];
+            for (int i = 0; i < maxn; i++)
+                for (int j = 1; j < maxn; j++)
+                    second[i][j - 1] += arr[i][j];
+        }
+        else if (a == 'U')
+            for (int i = 1; i < maxn; i++)
+                for (int j = 0; j < maxn; j++)
+                    second[i - 1][j] += arr[i][j];
+        else if (a == 'R')
+            for (int i = 0; i < maxn; i++)
+                for (int j = 0; j < maxn - 1; j++)
+                    second[i][j + 1] += arr[i][j];
+        else if (a == 'L')
+            for (int i = 0; i < maxn; i++)
+                for (int j = 1; j < maxn; j++)
+                    second[i][j - 1] += arr[i][j];
+        else if (a == 'D')
+            for (int i = 0; i < maxn - 1; i++)
+                for (int j = 0; j < maxn; j++)
+                    second[i + 1][j] += arr[i][j];
+
+        for (int i = 0; i < maxn; i++)
+            for (int j = 0; j < maxn; j++)
+                arr[i][j] = second[i][j];
+        memset(second, 0, sizeof second);
+    }
+    cout << arr[maxn - 1][0] << "\n";
 }
