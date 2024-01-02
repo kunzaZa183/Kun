8c61b92bbce2b65140a94c753590fa03cf3dd045
kunzaZa183 <yongvanichthee@gmail.com>
2024-01-02 09:46:33 +0700

    CSES

--- main2.cpp
+++ main2.cpp
@@ -1,49 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int maxn = (1 << 16);
+bitset<maxn> bs;
+void move(int length, int cur, int to, int canplace)
+{
+    if (length == 1)
+    {
+        cout << cur << " " << to << "\n";
+        return;
+    }
+    move(length - 1, cur, canplace, to);
+    cout << cur << " " << to << '\n';
+    move(length - 1, canplace, to, cur);
+}
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    string s;
-    cin >> s;
-    map<char, int> mci;
-    for (auto a : s)
-        mci[a]++;
-    int ct = 0;
-    for (auto a : mci)
-        if (a.second % 2 == 1)
-            ct++;
-    if (ct > 1)
-    {
-        cout << "NO SOLUTION\n";
-        return 0;
-    }
-
-    if (ct == 0)
-    {
-        string s1;
-        for (auto a : mci)
-            for (int i = 0; i < a.second / 2; i++)
-                s1.push_back(a.first);
-        cout << s1;
-        reverse(s1.begin(), s1.end());
-        cout << s1 << '\n';
-    }
-    else if (ct == 1)
-    {
-        pair<char, int> odd;
-        for (auto a : mci)
-            if (a.second % 2 == 1)
-                odd = a;
-        string s1;
-        for (auto a : mci)
-            if (a.second % 2 == 0)
-                for (int i = 0; i < a.second / 2; i++)
-                    s1.push_back(a.first);
-        cout << s1;
-        while (odd.second--)
-            cout << odd.first;
-        reverse(s1.begin(), s1.end());
-        cout << s1 << '\n';
-    }
+    int n;
+    cin >> n;
+    cout << (1 << n) - 1 << "\n";
+    move(n, 1, 3, 2);
 }
