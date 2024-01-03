7bea431cd7f3b121bfa1594da76a93a2c636e276
kunzaZa183 <yongvanichthee@gmail.com>
2024-01-03 13:05:48 +0700

    Attempt Trainto
    Failed & got 15 + TLE
    Looking at O(n^3) sol, possibly O(n^2logn)

--- main.cpp
+++ main.cpp
@@ -1,54 +1,32 @@
-// O(n^3logn)
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 350, BIGNUM = 100000;
-int arr[maxn + 1], qsum[maxn + 1], dp[maxn + 1][maxn + 1][maxn + 1];
-int k, x, i, j;
-void dcdp(int left, int right, int mini, int maxi)
-{
-
-    int mid = (mini + maxi) / 2, bestop = -1;
-    for (int y = mini; y <= maxi; y++)
-    {
-        int val = dp[k - 1][x + 1][y - 1] + (x - i) * (qsum[x] - qsum[i - 1]) + (j - y) * (qsum[j] - qsum[y - 1]) + 2 * (x - i + 1) * (j - y + 1);
-        if (val < dp[k][i][j])
-        {
-            dp[k][i][j] = val;
-            bestop = y;
-        }
-    }
-    if(left!=right)
-        dcdp(left)
-}
+const int maxn = 350;
+int dp[maxn + 1][maxn + 1], qsum[maxn+1], trace[maxn + 1][maxn + 1];
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n, actualk;
-    cin >> n >> actualk;
-    for (int i = 1; i <= n; i++)
-        cin >> arr[i];
-    sort(arr + 1, arr + 1 + n, greater<int>());
-    for (int i = 1; i <= n; i++)
-        qsum[i] += qsum[i - 1] + arr[i];
-    for (int i = 1; i <= maxn; i++)
-        for (int j = 1; j <= maxn; j++)
-            for (int k = 1; k <= maxn; k++)
-                dp[i][j][k] = BIGNUM;
-    for (int i = 1; i <= maxn; i++)
-        for (int j = i + 1; j <= maxn; j++)
-            for (int numleft = 0; numleft <= (j - i + 1); numleft++)
+    int n,m;
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)
+        cin>>qsum[i];
+    sort(qsum + 1,qsum + n + 1);
+    for(int i=1;i<=n;i++)
+        qsum[i] += qsum[i-1];
+    for(int i=0;i<m*2;i++)
+        for(int )
+    for(int j = 1;j<=n;j++)
+    {
+        dp[1][j] = qsum[j] * (j - 1); 
+        trace[1][j] = 1;
+    }
+    for(int i=2;i<=n;i++)
+        if(i<=m)
+        {
+            for(int j=i;j<=n;j++)
             {
-                int numright = j - i + 1 - numleft;
-                dp[1][i][j] = min(dp[1][i][j], (qsum[i - 1 + numleft] - qsum[i - 1]) * (numleft - 1) + (qsum[j] - qsum[i + numleft - 1]) * (numright - 1) + 2 * numleft * numright);
-            }
-    for (k = 2; k <= actualk; k++)
-        for (i = 1; i <= n; i++)
-            for (j = i + 2 * k - 1; j <= n; j++)
-                for (x = i; x < n / k + i + 1 && x < j; x++)
-                    for (int y = x + 1; y <= j; y++)
-                    {
-                        dp[k][i][j] = min(dp[k][i][j], dp[k - 1][x + 1][y - 1] + (x - i) * (qsum[x] - qsum[i - 1]) + (j - y) * (qsum[j] - qsum[y - 1]) + 2 * (x - i + 1) * (j - y + 1));
-                    }
-    cout << dp[actualk][1][n] << "\n";
+                for(int k=i-1;k<j;k++)
+                dp[i][j] = min(dp[i][j],)
+            }    
+        }   
 }
--- main2.cpp
+++ main2.cpp
@@ -1,52 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 7;
-int arr[maxn][maxn], second[maxn][maxn];
+
+long long arr[355], qs[355], dp[355][355], trace[355][355];
+
+int search(int ni, int nj, int ii)
+{
+	while(true)
+	{
+		if(ni == ii) return nj - trace[ni][nj] + 1;
+		nj = trace[ni][nj]-1;
+		ni--;
+	}
+}
+
+void solve(int i, int m, int l, int r, int a, int b)
+{
+	if(l > r) return ;
+	int mid = (l+r)>>1;
+	dp[i][mid] = 1e15;
+	for(int k=a ; k<=min(b, mid) ; k++)
+	{
+		long long table = (i > m) ? search(i-1, k-1, 2*m+1-i) : 0;
+		if(dp[i-1][k-1] + (qs[mid]-qs[k-1])*(mid-k) + 2*table*(mid-k+1) < dp[i][mid])
+		{
+			dp[i][mid] = dp[i-1][k-1] + (qs[mid]-qs[k-1])*(mid-k) + 2*table*(mid-k+1);
+			trace[i][mid] = k;
+		}
+	}
+	solve(i, m, l, mid-1, a, trace[i][mid]);
+	solve(i, m, mid+1, r, trace[i][mid], b);
+}
+
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    string s;
-    cin >> s;
-    arr[0][0] = 1;
-    for (auto a : s)
-    {
-        if (a == '?')
-        {
-            for (int i = 0; i < maxn; i++)
-                for (int j = 0; j < maxn - 1; j++)
-                    second[i][j + 1] += arr[i][j];
-            for (int i = 0; i < maxn - 1; i++)
-                for (int j = 0; j < maxn; j++)
-                    second[i + 1][j] += arr[i][j];
-            for (int i = 1; i < maxn; i++)
-                for (int j = 0; j < maxn; j++)
-                    second[i - 1][j] += arr[i][j];
-            for (int i = 0; i < maxn; i++)
-                for (int j = 1; j < maxn; j++)
-                    second[i][j - 1] += arr[i][j];
-        }
-        else if (a == 'U')
-            for (int i = 1; i < maxn; i++)
-                for (int j = 0; j < maxn; j++)
-                    second[i - 1][j] += arr[i][j];
-        else if (a == 'R')
-            for (int i = 0; i < maxn; i++)
-                for (int j = 0; j < maxn - 1; j++)
-                    second[i][j + 1] += arr[i][j];
-        else if (a == 'L')
-            for (int i = 0; i < maxn; i++)
-                for (int j = 1; j < maxn; j++)
-                    second[i][j - 1] += arr[i][j];
-        else if (a == 'D')
-            for (int i = 0; i < maxn - 1; i++)
-                for (int j = 0; j < maxn; j++)
-                    second[i + 1][j] += arr[i][j];
-
-        for (int i = 0; i < maxn; i++)
-            for (int j = 0; j < maxn; j++)
-                arr[i][j] = second[i][j];
-        memset(second, 0, sizeof second);
-    }
-    cout << arr[maxn - 1][0] << "\n";
+	int n, m;
+	scanf(" %d %d",&n,&m);
+	for(int i=1 ; i<=n ; i++)
+		scanf(" %lld",&arr[i]);
+	sort(arr+1, arr+n+1);
+	for(int i=1 ; i<=n ; i++)
+	{
+		qs[i] = arr[i] + qs[i-1];
+		dp[1][i] = qs[i] * (i-1);
+		trace[1][i] = 1;
+	}
+	for(int i=2 ; i<=m*2 ; i++)
+		solve(i, m, 1, n, 1, n);
+	printf("%lld\n",dp[2*m][n]);
+	return 0;
 }
