13bf5b5ae26f37b137abf0de62c2fa767dcf2a47
kunzaZa183 <yongvanichthee@gmail.com>
2024-01-04 22:29:13 +0700

    Racing seems like hell

--- main.cpp
+++ main.cpp
@@ -1,64 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-const int maxn = 200000;
+const int maxn = 100000,color = 5;
 int arr[maxn];
-signed main()
+int segtree[color][4*maxn];
+int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int tests;
-    cin >> tests;
-    while (tests--)
-    {
-        int n, qs;
-        cin >> n >> qs;
-        for (int i = 0; i < n; i++)
-            cin >> arr[i];
-        int sum = accumulate(arr, arr + n, 0LL);
-        int actualsum = sum;
-        int mini = arr[n - 1];
-        for (int i = n - 2; i >= 0; i--)
-        {
-            if (mini > arr[i])
-            {
-                sum -= arr[i];
-                mini -= arr[i];
-                mini = min(mini, arr[i]);
-            }
-            else
-            {
-                sum -= mini;
-                mini = 0;
-                break;
-            }
-        }
-        cout << sum << "\n";
-        while (qs--)
-        {
-            int in, val;
-            cin >> in >> val;
-            in--;
-            actualsum += val - arr[in];
-            arr[in] = val;
-            sum = actualsum;
-            mini = arr[n - 1];
-            for (int i = n - 2; i >= 0; i--)
-            {
-                if (mini > arr[i])
-                {
-                    sum -= arr[i];
-                    mini -= arr[i];
-                    mini = min(mini, arr[i]);
-                }
-                else
-                {
-                    sum -= mini;
-                    mini = 0;
-                    break;
-                }
-            }
-            cout << sum << "\n";
-        }
-    }
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+     
 }
