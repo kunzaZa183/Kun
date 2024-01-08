8b61b148b654521fa49eecc439c4ae63d69fc723
kunzaZa183 <yongvanichthee@gmail.com>
2024-01-08 16:18:54 +0700

    Fun CSES

--- main.cpp
+++ main.cpp
@@ -1,46 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 500;
-string plain[maxn], spotty[maxn];
+const int maxn = 200000;
+int arr[maxn];
 int main()
 {
-    freopen("cownomics.in", "r", stdin);
-    freopen("cownomics.out", "w", stdout);
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n, len;
-    cin >> n >> len;
+    int n, x;
+    cin >> n >> x;
+    multiset<int> msi;
     for (int i = 0; i < n; i++)
-        cin >> plain[i];
-    for (int i = 0; i < n; i++)
-        cin >> spotty[i];
+    {
+        int tmp;
+        cin >> tmp;
+        msi.insert(tmp);
+    }
     int ct = 0;
-    for (int j = 0; j < len; j++)
-        for (int k = j + 1; k < len; k++)
-            for (int l = k + 1; l < len; l++)
-            {
-                set<string> si1, si2;
-                for (int i = 0; i < n; i++)
-                {
-                    string tmp;
-                    tmp.push_back(plain[i][j]);
-                    tmp.push_back(plain[i][k]);
-                    tmp.push_back(plain[i][l]);
-                    si1.insert(tmp);
-                }
-                for (int i = 0; i < n; i++)
-                {
-                    string tmp;
-                    tmp.push_back(spotty[i][j]);
-                    tmp.push_back(spotty[i][k]);
-                    tmp.push_back(spotty[i][l]);
-                    si2.insert(tmp);
-                }
-                for (auto a : si1)
-                    if (si2.count(a))
-                        goto A;
-                ct++;
-            A:;
-            }
+    while (!msi.empty())
+    {
+        auto it = msi.begin();
+        auto target = msi.upper_bound(x - *it);
+        if (target != msi.begin())
+        {
+            target--;
+            if (target != msi.begin())
+                msi.erase(target);
+        }
+        msi.erase(msi.begin());
+        ct++;
+    }
     cout << ct << "\n";
 }
