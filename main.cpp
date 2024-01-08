25443c5e75f2e68dc7bbba360f0811c75dac62e7
kunzaZa183 <yongvanichthee@gmail.com>
2024-01-08 13:34:51 +0700

    Fun USACO

--- main.cpp
+++ main.cpp
@@ -1,31 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-int paint[2000][2000];
+const int maxn = 500;
+string plain[maxn], spotty[maxn];
 int main()
 {
-    freopen("billboard.in", "r", stdin);
-    freopen("billboard.out", "w", stdout);
+    freopen("cownomics.in", "r", stdin);
+    freopen("cownomics.out", "w", stdout);
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int arr[8];
-    for (int i = 0; i < 8; i++)
-    {
-        cin >> arr[i];
-        arr[i] += 1000;
-    }
-    for (int i = 0; i < 2; i++)
-        for (int j = arr[4 * i]; j < arr[4 * i + 2]; j++)
-            for (int k = arr[4 * i + 1]; k < arr[4 * i + 3]; k++)
-                paint[j][k] = int(!i);
-    int minx = 2000, maxx = 0, miny = 2000, maxy = 0;
-    for (int i = 0; i < 2000; i++)
-        for (int j = 0; j < 2000; j++)
-            if (paint[i][j])
+    int n, len;
+    cin >> n >> len;
+    for (int i = 0; i < n; i++)
+        cin >> plain[i];
+    for (int i = 0; i < n; i++)
+        cin >> spotty[i];
+    int ct = 0;
+    for (int j = 0; j < len; j++)
+        for (int k = j + 1; k < len; k++)
+            for (int l = k + 1; l < len; l++)
             {
-                minx = min(minx, i);
-                maxx = max(maxx, i);
-                miny = min(miny, j);
-                maxy = max(maxy, j);
+                set<string> si1, si2;
+                for (int i = 0; i < n; i++)
+                {
+                    string tmp;
+                    tmp.push_back(plain[i][j]);
+                    tmp.push_back(plain[i][k]);
+                    tmp.push_back(plain[i][l]);
+                    si1.insert(tmp);
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    string tmp;
+                    tmp.push_back(spotty[i][j]);
+                    tmp.push_back(spotty[i][k]);
+                    tmp.push_back(spotty[i][l]);
+                    si2.insert(tmp);
+                }
+                for (auto a : si1)
+                    if (si2.count(a))
+                        goto A;
+                ct++;
+            A:;
             }
-    cout << max(0, maxx - minx + 1) * max(0, maxy - miny + 1) << "\n";
+    cout << ct << "\n";
 }
