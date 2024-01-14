d1b8776a25fe3895e99b19982926cb3518db305d
kunzaZa183 <yongvanichthee@gmail.com>
2024-01-14 12:05:50 +0700

    CF

--- main3.cpp
+++ main3.cpp
@@ -1,79 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+struct kun
+{
+    int bef, rep;
+    vector<int> newnum;
+    friend bool operator<(kun a, kun b)
+    {
+        return a.bef < b.bef;
+    }
+};
 int32_t main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int tests;
-	cin >> tests;
-	while (tests--)
-	{
-		int n, q;
-		cin >> n >> q;
-		vector<tuple<vector<int>, int, int>> vpivii;
-		int lastc = 0;
-		vpivii.emplace_back(vector<int>(), 1, 0);
-		for (int i = 0; i < n; i++)
-		{
-			int tp, x;
-			cin >> tp >> x;
-
-			auto& [a, b, c] = vpivii.back();
-			if (tp == 1)
-			{
-				a.push_back(x);
-				c++;
-			}
-			else
-			{
-				b = x + 1;
-				c = (lastc + a.size()) * b;
-				if (c < lastc)
-					c = LLONG_MAX;
-				lastc = c;
-				vpivii.emplace_back(vector<int>(), 1, c);
-			}
-		}
-		for (int i = 0; i < q; i++)
-		{
-			int query;
-			cin >> query;
-			query--;
-			while (1)
-			{
-				for (int i = 0; i < vpivii.size(); i++)
-				{
-					auto& [a, b, c] = vpivii[i];
-					if (c > query)
-					{
-						if (i == 0)
-						{
-							query %= c / b;
-							if (query >= c / b - a.size())
-							{
-								cout << a[query - (c / b - a.size())] << ' ';
-								goto A;
-							}
-							else
-								break;
-						}
-						else
-						{
-							query %= get<2>(vpivii[i - 1]) + a.size();
-							if (query >= get<2>(vpivii[i - 1]))
-							{
-								cout << a[query - (get<2>(vpivii[i - 1]))] << ' ';
-								goto A;
-							}
-							else
-								break;
-						}
-					}
-				}
-			}
-		A:;
-		}
-		cout << '\n';
-	}
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int tests;
+    cin >> tests;
+    while (tests--)
+    {
+        int n, q;
+        cin >> n >> q;
+        vector<kun> vpivii;
+        int lastc = 0;
+        kun tmp;
+        tmp.bef = 0;
+        tmp.rep = 1;
+        vpivii.push_back(tmp);
+        for (int i = 0; i < n; i++)
+        {
+            int tp, x;
+            cin >> tp >> x;
+            if (tp == 1)
+                vpivii.back().newnum.push_back(x);
+            else
+            {
+                vpivii.back().rep = x + 1;
+                kun tmp;
+                if (vpivii.back().bef == LLONG_MAX)
+                    tmp.bef = LLONG_MAX;
+                else if (log10(vpivii.back().bef + vpivii.back().newnum.size()) + log10(vpivii.back().rep) > 18.3)
+                    tmp.bef = LLONG_MAX;
+                else
+                    tmp.bef = (vpivii.back().bef + vpivii.back().newnum.size()) * vpivii.back().rep;
+                tmp.rep = 1;
+                vpivii.push_back(tmp);
+            }
+        }
+        while (vpivii.back().bef == LLONG_MAX)
+            vpivii.pop_back();
+        for (int i = 0; i < q; i++)
+        {
+            int query;
+            cin >> query;
+            query--;
+            for (int i = vpivii.size() - 1; i >= 0; i--)
+            {
+                query %= vpivii[i].bef + vpivii[i].newnum.size();
+                if (query >= vpivii[i].bef)
+                {
+                    cout << vpivii[i].newnum[query - vpivii[i].bef] << ' ';
+                    break;
+                }
+            }
+        }
+        cout << '\n';
+    }
 }
