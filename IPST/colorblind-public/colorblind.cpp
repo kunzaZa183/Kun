45c35b20b55459824e9f60338f176e1190738a16
kunzaZa183 <yongvanichthee@gmail.com>
2024-01-15 12:29:38 +0700

    Check correctness of table pls

--- IPST/colorblind-public/colorblind.cpp
+++ IPST/colorblind-public/colorblind.cpp
@@ -1,35 +1,8 @@
 #include "colorblind.h"
 #include <bits/stdc++.h>
 using namespace std;
-const string tmp = "RB";
-int findval(string &s)
-{
-  for(auto a:s)
-}
-bool check(string s, vector<int> diff)
-{
-  int ct = 0;
-  for (auto a : s)
-    if (a == 'R')
-      ct++;
-    else if (a == 'B')
-      ct--;
-  while (ct > 0)
-  {
-    s.push_back('B');
-    ct--;
-  }
-  while (ct < 0)
-  {
-    s.push_back('R');
-    ct++;
-  }
-
-}
 string investivate_colors(int N)
 {
-  string ans;
-  ans.push_back('R');
   vector<int> vi;
   for (int i = 0; i < 3; i++)
     for (int j = i + 1; j < 3; j++)
@@ -39,14 +12,20 @@ string investivate_colors(int N)
     }
   vector<int> diff;
   for (int i = 0; i < vi.size(); i++)
-    diff.push_back(vi[i + 1] - vi[i]);
-  for (auto b : tmp)
-    for (auto a : tmp)
-    {
-      ans.push_back(b);
-      ans.push_back(a);
-
-      ans.pop_back();
-      ans.pop_back();
-    }
+    diff.push_back(vi[i + 1] - vi[0]);
+  vector<int> ans;
+  if (diff == vector<int>{0, 0})
+    ans = {1, 1, 1};
+  else if (diff == vector<int>{-2, -2})
+    ans = {1, 1, -1};
+  else if (diff == vector<int>{0, 2})
+    ans = {1, -1, 1};
+  else if (diff == vector<int>{2, 0})
+    ans = {1, -1, -1};
+  int pref = 0;
+  for (int i = 3; i < 2 * N; i++)
+  {
+    pref += ans[i - 3];
+    
+  }
 }
