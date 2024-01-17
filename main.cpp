623adc512c8e6bda17755254ffd5b941bb034e16
kunzaZa183 <yongvanichthee@gmail.com>
2024-01-17 19:41:39 +0700

    0 Coins

--- main.cpp
+++ main.cpp
@@ -1,71 +1,102 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 100;
-tuple<int, int, int> tiii[maxn];
-int lefty[maxn];
+const int maxn = 1000000, BIGNUM = 1e9 + 1;
+int type[maxn], weight[maxn], qsum[maxn];
+vector<int> all[maxn + 1];
+int dp[maxn][2];
+int n;
+int sumright(int a, int b)
+{
+  if (a < b)
+  {
+    if (a == 0)
+      return qsum[b - 1];
+    return qsum[b - 1] - qsum[a - 1];
+  }
+  if (a == b)
+    return 0;
+  int front;
+  if (b == 0)
+    front = 0;
+  else
+    front = qsum[b - 1];
+  int back = qsum[n - 1] - qsum[a - 1];
+  return back + front;
+}
 int main()
 {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
-  freopen("tracing.in", "r", stdin);
-  freopen("tracing.out", "w", stdout);
-  int n, t;
-  cin >> n >> t;
-  string corr;
-  cin >> corr;
-  for (int i = 0; i < t; i++)
-  {
-    auto &[a, b, c] = tiii[i];
-    cin >> a >> b >> c;
-    b--, c--;
-  }
-  sort(tiii, tiii + t);
-  int mini = INT_MAX, maxi = INT_MIN, ct = 0;
+  int n;
+  cin >> n;
+  int maxtype = -1;
   for (int i = 0; i < n; i++)
+    cin >> type[i];
+  for (int i = 0; i < n; i++)
+    cin >> weight[i];
+  qsum[0] = weight[0];
+  for (int i = 1; i < n; i++)
+    qsum[i] = qsum[i - 1] + weight[i];
+  for (int i = 0; i < n; i++)
+    maxtype = max(maxtype, type[i]);
+  for (int i = 0; i < n; i++)
+    all[type[i]].push_back(i);
+  int in = 0;
+  int sumall = accumulate(weight, weight + n, 0);
+  fill(dp, dp + n, BIGNUM);
+  for (auto a : all[1])
+    dp[a][0] = 0;
+  for (int i = 1; i < maxtype; i++)
   {
-    bool works = 0;
-    for (int tmpk = 0; tmpk <= n + 1; tmpk++)
+    if (all[i].size() == 1)
+      dp[all[i][0]][1] = dp[all[i][0]][0];
+    else
     {
-      string s(n, '0');
-      memset(lefty, 0, sizeof lefty);
-      lefty[i] = tmpk;
-      s[i] = '1';
-      for (int j = 0; j < t; j++)
+      for (int j = 1; j < all[i].size(); j++)
       {
-        auto [a, b, c] = tiii[j];
-        if (lefty[b] > 0 || lefty[c] > 0)
-        {
-          if (s[b] == '1' && s[c] == '1')
-          {
-            lefty[b]--;
-            lefty[c]--;
-          }
-          else if (s[b] == '1')
-          {
-            s[c] = '1';
-            lefty[c] = tmpk;
-            lefty[b]--;
-          }
-          else if (s[c] == '1')
-          {
-            s[b] = '1';
-            lefty[b] = tmpk;
-            lefty[c]--;
-          }
-        }
+        dp[all[i][j - 1]][1] = min(dp[all[i][j - 1]][1], sumall - weight[all[i][j - 1]] + dp[all[i][j]][0]);
+        dp[all[i][j]][1] = min(dp[all[i][j]][1], sumall - weight[all[i][j - 1]] + dp[all[i][j - 1]][0]);
       }
-      if (s == corr)
-      {
-        works = 1;
-        mini = min(mini, tmpk);
-        maxi = max(maxi, tmpk);
-      }
+      dp[all[i][0]][1] = min(dp[all[i][0]][1], sumall - weight[all[i].back()] + dp[all[i].back()][0]);
+      dp[all[i].back()][1] = min(dp[all[i].back()][1], sumall - weight[all[i].back()] + dp[all[i][0]][0]);
     }
-    ct += works;
+
+    vector<int> twolevel;
+    for (auto a : all[i])
+      twolevel.push_back(a);
+    for (auto a : all[i + 1])
+      twolevel.push_back(a);
+    int n = twolevel.size();
+    for (int j = 0; j < n; j++)
+      twolevel.push_back(twolevel[j]);
+
+    for (int j = 0; j < 2; j++)
+    {
+      int cur = -1;
+      for (auto a : twolevel)
+        if (type[a] == i)
+          cur = a;
+        else if (cur != -1)
+        {
+          if (j == 0)
+            dp[a][0] = min(dp[a][0], dp[cur][1] + sumright(cur, a));
+          else if (j == 1)
+            dp[a][0] = min(dp[a][0], dp[cur][1] + sumright(a, cur));
+        }
+      reverse(twolevel.begin(), twolevel.end());
+    }
   }
-  cout << ct << ' ' << mini << " ";
-  if (maxi == n + 1)
-    cout << "Infinity\n";
-  else
-    cout << maxi << "\n";
+
+  for (int j = 1; j < all[maxtype].size(); j++)
+  {
+    dp[all[maxtype][j - 1]][1] = min(dp[all[maxtype][j - 1]][1], sumall - weight[all[maxtype][j - 1]] + dp[all[maxtype][j]][0]);
+    dp[all[maxtype][j]][1] = min(dp[all[maxtype][j]][1], sumall - weight[all[maxtype][j - 1]] + dp[all[maxtype][j - 1]][0]);
+  }
+  dp[all[maxtype][0]][1] = min(dp[all[maxtype][0]][1], sumall - weight[all[maxtype].back()] + dp[all[maxtype].back()][0]);
+  dp[all[maxtype].back()][1] = min(dp[all[maxtype].back()][1], sumall - weight[all[maxtype].back()] + dp[all[maxtype][0]][0]);
+
+  int mini = INT_MAX;
+  for (auto a : all[maxtype])
+    mini = min(mini, dp[a][1]);
+  cout << mini << '\n';
 }
