0eef4804bd4045f872763f3259ee11d28e50614d
kunzaZa183 <yongvanichthee@gmail.com>
2024-01-20 14:50:58 +0700

    Helped Jason

--- main5.cpp
+++ main5.cpp
@@ -1,31 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int maxm = 1000000;
+pair<int, int> arr[maxm];
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int len;
-    cin >> len;
-    string s;
-    cin >> s;
-    int sum = 0;
-    for (int i = 1; i <= len; i++)
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+  int n, d, m;
+  cin >> n >> d >> m;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i].first;
+    arr[i].first--;
+    arr[i].second = i;
+  }
+  sort(arr, arr + m);
+  int l = 1, r = 2000000;
+  while (l < r)
+  {
+    int mid = (l + r) / 2;
+    int in = 0;
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j <= len - i; j++)
+      if (arr[in].first < i - d)
+        goto A;
+      int ct = 0;
+      while (ct < mid && in < m)
+      {
+        if (arr[in].first <= i)
         {
-            string tmp = s.substr(j, i);
-            int maxt = 0;
-            int curt = 0;
-            for (auto a : tmp)
-                if (a == 'T')
-                {
-                    curt++;
-                    maxt = max(maxt, curt);
-                }
-                else if (a == 'F')
-                    curt = 0;
-            sum += maxt;
+          ct++;
+          in++;
         }
+        else
+          break;
+      }
     }
-    cout << sum << '\n';
+    if (in == m)
+    {
+      r = mid;
+      continue;
+    }
+  A:;
+    l = mid + 1;
+  }
+  cout << l << "\n";
+  int in2 = 0;
+  for (int i = 0; i < n; i++)
+  {
+    int ct = 0;
+    while (ct < l && in2 < m)
+    {
+      if (arr[in2].first <= i)
+      {
+        cout << arr[in2].second + 1 << ' ';
+        ct++;
+        in2++;
+      }
+      else
+        break;
+    }
+    cout << "0\n";
+  }
 }
