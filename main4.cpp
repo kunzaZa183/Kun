4f6a5d3682d5f9515693271abea291d4c087207b
kunzaZa183 <yongvanichthee@gmail.com>
2024-01-23 19:41:16 +0700

    Upsolve Atcoder and Fun USACO

--- main4.cpp
+++ main4.cpp
@@ -1,24 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 200000;
-pair<int, int> arr[maxn];
+const int maxn = 100;
+vector<int> adjlist[maxn];
+bitset<maxn> bsmn;
+void dfs(int cur)
+{
+  bsmn[cur] = 1;
+  for (auto a : adjlist[cur])
+    dfs(a);
+}
 int main()
 {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
+  freopen("factory.in", "r", stdin);
+  freopen("factory.out", "w", stdout);
   int n;
   cin >> n;
+  for (int i = 0; i < n - 1; i++)
+  {
+    int a, b;
+    cin >> a >> b;
+    a--, b--;
+    adjlist[b].push_back(a);
+  }
   for (int i = 0; i < n; i++)
-    cin >> arr[i].first >> arr[i].second;
-  sort(arr, arr + n, [](pair<int, int> a, pair<int, int> b)
-       { return a.second < b.second; });
-  int ct = 0;
-  int cur = -1;
-  for (int i = 0; i < n; i++)
-    if (arr[i].first >= cur)
-    {
-      cur = arr[i].second;
-      ct++;
-    }
-  cout << ct << '\n';
+  {
+    bsmn.reset();
+    dfs(i);
+    for (int j = 0; j < n; j++)
+      if (!bsmn[j])
+        goto A;
+    cout << i + 1 << '\n';
+    return 0;
+  A:;
+  }
+  cout << "-1\n";
 }
