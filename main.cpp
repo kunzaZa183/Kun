335d6c4a9f1d598d14b6733ffd834869557fdab5
kunzaZa183 <yongvanichthee@gmail.com>
2024-01-30 19:17:30 +0700

    1 Usaco

--- main.cpp
+++ main.cpp
@@ -1,45 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 2001;
-int arr[maxn][maxn], qsum[maxn][maxn], toright[maxn][maxn], todown[maxn][maxn];
+#define int long long
+const int maxn = 200000;
+int arr[maxn];
 int main()
 {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
-  int n, m, q;
-  cin >> n >> m >> q;
-  for (int i = 1; i <= n; i++)
+  int tests;
+  cin >> tests;
+  while (tests--)
   {
-    string s;
-    cin >> s;
-    for (int j = 1; j <= m; j++)
-      arr[i][j] = s[j - 1] - '0';
-  }
-
-  for (int i = 1; i <= n; i++)
-    for (int j = 1; j < m; j++)
-      toright[i][j] = arr[i][j] && arr[i][j + 1];
-
-  for (int i = 1; i < n; i++)
-    for (int j = 1; j <= m; j++)
-      todown[i][j] = arr[i][j] && arr[i + 1][j];
-
-  for (int i = 1; i <= n; i++)
-    for (int j = 1; j <= m; j++)
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+      cin >> arr[i];
+    sort(arr, arr + n);
+    int l = 1, r = 1e11;
+    while (l < r)
     {
-      qsum[i][j] = arr[i][j] + qsum[i - 1][j] + qsum[i][j - 1] - qsum[i - 1][j - 1];
-      toright[i][j] += toright[i - 1][j] + toright[i][j - 1] - toright[i - 1][j - 1];
-      todown[i][j] += todown[i - 1][j] + todown[i][j - 1] - todown[i - 1][j - 1];
+      int mid = (l + r) / 2;
+      int ct = 0;
+      for (int i = 0; i < n; i++)
+        if (arr[i] > mid)
+        {
+          ct++;
+        }
+      int total = 0;
+      int maxi = 1;
+      for (int i = 1; 1; i++)
+      {
+        total += i;
+        if (total > mid)
+        {
+          total -= i;
+          maxi = i;
+          break;
+        }
+      }
+      vector<int> vi;
+      while (total != 0)
+      {
+        vi.push_back(total);
+        total -= maxi++;
+      }
+      
     }
-
-  while (q--)
-  {
-    pair<int, int> topl, botr;
-    cin >> topl.first >> topl.second >> botr.first >> botr.second;
-    int blues = qsum[botr.first][botr.second] - qsum[topl.first - 1][botr.second] - qsum[botr.first][topl.second - 1] + qsum[topl.first - 1][topl.second - 1];
-    int connect = 0;
-    connect += toright[botr.first][botr.second - 1] - toright[botr.first][topl.second - 1] - toright[topl.first - 1][botr.second - 1] + toright[topl.first - 1][topl.second - 1];
-    connect += todown[botr.first - 1][botr.second] - todown[botr.first - 1][topl.second - 1] - todown[topl.first - 1][botr.second] + todown[topl.first - 1][topl.second - 1];
-    cout << blues - connect << '\n';
   }
 }
--- main2.cpp
+++ main2.cpp
@@ -1,53 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 1000000, BIGNUM = 1e9 + 1;
-int type[maxn], weight[maxn], qsum[maxn];
-int n;
-int sumright(int a, int b)
-{
-  if (a < b)
-  {
-    if (a == 0)
-      return qsum[b - 1];
-    return qsum[b - 1] - qsum[a - 1];
-  }
-  if (a == b)
-    return 0;
-  int front;
-  if (b == 0)
-    front = 0;
-  else
-    front = qsum[b - 1];
-  int back = qsum[n - 1] - qsum[a - 1];
-  return back + front;
-}
 int main()
 {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
-  cin >> n;
-  for (int i = 0; i < n; i++)
-    cin >> type[i];
-  for (int i = 0; i < n; i++)
-    cin >> weight[i];
-  qsum[0] = weight[0];
-  for (int i = 1; i < n; i++)
-    qsum[i] = qsum[i - 1] + weight[i];
-  vector<int> vi;
-  for (int i = 0; i < n; i++)
-    vi.push_back(i);
-  int mini = INT_MAX;
-  do
-  {
-
-    int sum = 0;
-    for (int i = 1; i < vi.size(); i++)
-      if (type[vi[i - 1]] > type[vi[i]])
-        goto A;
-    for (int i = 1; i < vi.size(); i++)
-      sum += min(sumright(vi[i - 1], vi[i]), sumright(vi[i], vi[i - 1]));
-    mini = min(mini, sum);
-  A:;
-  } while (next_permutation(vi.begin(), vi.end()));
-  cout << mini << '\n';
+  cout<<"6\n";
 }
