57514505b0d49e341567e1c3a30c223e95984ce5
kunzaZa183 <yongvanichthee@gmail.com>
2024-02-02 21:51:42 +0700

    AndAndAnd

--- main2.cpp
+++ main2.cpp
@@ -4,5 +4,58 @@ int main()
 {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
-  cout<<"6\n";
+  string s;
+  cin >> s;
+  string order;
+  string ourstack;
+  for (int i = s.size() - 1; i >= 0; i--)
+    if (s[i] == ')')
+      ourstack.push_back(')');
+    else if (s[i] == '(')
+    {
+      string tmp;
+      while (ourstack.back() != ')')
+      {
+        tmp.push_back(ourstack.back());
+        ourstack.pop_back();
+      }
+      ourstack.pop_back();
+      for (int j = tmp.size() - 1; j >= 0; j--)
+        order.push_back(tmp[j]);
+    }
+    else if (s[i] == '|')
+    {
+      while (!ourstack.empty())
+        if (ourstack.back() == '|')
+        {
+          order.push_back('|');
+          ourstack.pop_back();
+          break;
+        }
+        else if (ourstack.back() == '&')
+        {
+          order.push_back('&');
+          ourstack.pop_back();
+        }
+        else
+          break;
+      ourstack.push_back('|');
+    }
+    else if (s[i] == '&')
+    {
+      if (ourstack.empty())
+        ourstack.push_back('&');
+      else if (ourstack.back() == '&')
+        order.push_back('&');
+      else
+        ourstack.push_back('&');
+    }
+  for (int i = ourstack.size() - 1; i >= 0; i--)
+    order.push_back(ourstack[i]);
+  for (int i = 0; i < order.size(); i++)
+  {
+    if (i != 0)
+      cout << " ";
+    cout << order[i];
+  }
 }
