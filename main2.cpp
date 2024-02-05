29848f02c0d4b3b51912da1c508a8dc5075d1731
kunzaZa183 <yongvanichthee@gmail.com>
2024-02-05 18:48:30 +0700

    ๊Upsolve Bangmod

--- main2.cpp
+++ main2.cpp
@@ -1,12 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int maxn = 1000;
-struct bomb
-{
-  int x, y, radius;
-  int begin, end;
-} allbomb[maxn];
-int segtree[4 * maxn][4 * maxn];
+bool segtree[4 * maxn][4 * maxn];
 int n, m;
 void updatey(int inx, int curiny, int curl, int curr, int lefty, int righty)
 {
@@ -14,12 +9,12 @@ void updatey(int inx, int curiny, int curl, int curr, int lefty, int righty)
     return;
   if (lefty <= curl && curr <= righty)
   {
-    segtree[inx][curiny] = (curr - curl + 1);
+    segtree[inx][curiny] = 1;
     return;
   }
   updatey(inx, curiny * 2, curl, (curl + curr) / 2, lefty, righty);
   updatey(inx, curiny * 2 + 1, (curl + curr) / 2 + 1, curr, lefty, righty);
-  segtree[inx][curiny] = segtree[inx][curiny * 2] + segtree[inx][curiny * 2 + 1];
+  segtree[inx][curiny] = segtree[inx][curiny * 2] && segtree[inx][curiny * 2 + 1];
 }
 void updatex(int curin, int curl, int curr, int leftx, int rightx, int topy, int boty)
 {
@@ -28,12 +23,13 @@ void updatex(int curin, int curl, int curr, int leftx, int rightx, int topy, int
   if (leftx <= curl && curr <= rightx)
   {
     updatey(curin, 1, 0, m - 1, topy, boty);
-    segtree[curin][0] = segtree[curin][1] + segtree[curin * 2 + 1][0] + segtree[curin * 2 + 2][0];
-    return;
+    if(segtree[curin][1])
+      segtree[curin][0] = 1;
   }
   updatex(curin * 2 + 1, curl, (curl + curr) / 2, leftx, rightx, topy, boty);
   updatex(curin * 2 + 2, (curl + curr) / 2 + 1, curr, leftx, rightx, topy, boty);
-  segtree[curin][0] = segtree[curin][1] + segtree[curin * 2 + 1][0] + segtree[curin * 2 + 2][0];
+  
+  segtree[curin][0] = segtree[curin * 2 + 1][0] && segtree[curin * 2 + 2][0];
 }
 int main()
 {
