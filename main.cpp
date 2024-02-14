98680cae26589be0a8e9b07708a13cd5bdc14d89
kunzaZa183 <yongvanichthee@gmail.com>
2024-02-14 19:21:35 +0700

    100 on Drilling

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
 const int maxn = 500000, days = 100;
-int arr[maxn], dp[maxn][days + 1];
-int main()
+int arr[maxn], dp[maxn][2];
+    vector<int> vi;
+signed main()
 {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
@@ -18,22 +20,23 @@ int main()
   }
   for (int i = 2; i <= k; i++)
   {
-    vector<int> vi;
+    vi.clear();
+    int cur = i%2;
     for (int j = i - 1; j < n; j++)
     {
-      dp[j][i] = dp[j-1][i-1] + arr[j];
+      dp[j][cur] = dp[j-1][1 - cur] + arr[j];
       while (!vi.empty())
-        if (arr[j] > arr[vi.back()])
+        if (arr[j] >= arr[vi.back()])
         {
-          dp[j][i] = min(dp[j][i], dp[vi.back()][i] - arr[vi.back()] + arr[j]);
+          dp[j][cur] = min(dp[j][cur], dp[vi.back()][cur] - arr[vi.back()] + arr[j]);
           vi.pop_back();
         }
         else
           break;
       if (!vi.empty())
-        dp[j][i] = min(dp[j][i], dp[vi.back()][i] + arr[j]);
+        dp[j][cur] = min(dp[j][cur], dp[vi.back()][cur]);
       vi.push_back(j);
     }
   }
-  cout << dp[n - 1][k] << "\n";
+  cout << dp[n - 1][k%2] << "\n";
 }
