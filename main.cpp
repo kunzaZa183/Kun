81e163dadf27f72349353776fcf86bbb210241ba
kunzaZa183 <75902969+kunzaZa183@users.noreply.github.com>
2024-02-14 08:49:31 +0000

    Drilling?

--- main.cpp
+++ main.cpp
@@ -1,29 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 1000000;
-int qsum[maxn + 1];
+const int maxn = 500000, days = 100;
+int arr[maxn], dp[maxn][days + 1];
 int main()
 {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
   int n, k;
   cin >> n >> k;
-  for (int i = 0; i < k; i++)
-  {
-    int a, b;
-    cin >> a >> b;
-    a--, b--;
-    qsum[a]++;
-    qsum[b + 1]--;
-  }
-  vector<int> vi;
-  int cur = 0;
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+  int maxi = -1;
   for (int i = 0; i < n; i++)
   {
-    cur += qsum[i];
-    vi.push_back(cur);
-    // cout << cur << '\n';
+    maxi = max(maxi, arr[i]);
+    dp[i][1] = maxi;
   }
-  sort(vi.begin(), vi.end());
-  cout << vi[vi.size() / 2] << "\n";
+  for (int i = 2; i <= k; i++)
+  {
+    vector<int> vi;
+    for (int j = i - 1; j < n; j++)
+    {
+      dp[j][i] = dp[j-1][i-1] + arr[j];
+      while (!vi.empty())
+        if (arr[j] > arr[vi.back()])
+        {
+          dp[j][i] = min(dp[j][i], dp[vi.back()][i] - arr[vi.back()] + arr[j]);
+          vi.pop_back();
+        }
+        else
+          break;
+      if (!vi.empty())
+        dp[j][i] = min(dp[j][i], dp[vi.back()][i] + arr[j]);
+      vi.push_back(j);
+    }
+  }
+  cout << dp[n - 1][k] << "\n";
 }
