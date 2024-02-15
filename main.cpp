ea94b215477b9025ee419761e37f2b3438731b84
kunzaZa183 <75902969+kunzaZa183@users.noreply.github.com>
2024-02-15 08:08:06 +0000

    Icy Problem

--- main.cpp
+++ main.cpp
@@ -1,39 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 500000, days = 100;
-int arr[maxn], dp[maxn][days + 1];
+const int maxm = 500, maxn = 1000001;
+vector<int> vi[maxm];
+int adjmatrix[maxm][maxm];
+vector<int> all[maxn];
 int main()
 {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
-  int n, k;
-  cin >> n >> k;
-  for (int i = 0; i < n; i++)
-    cin >> arr[i];
-  int maxi = -1;
-  for (int i = 0; i < n; i++)
+  int n, m;
+  cin >> n >> m;
+  for (int i = 0; i < m; i++)
   {
-    maxi = max(maxi, arr[i]);
-    dp[i][1] = maxi;
-  }
-  for (int i = 2; i <= k; i++)
-  {
-    vector<int> vi;
-    for (int j = i - 1; j < n; j++)
+    int x;
+    cin >> x;
+    vi[i].resize(x);
+    for (auto &a : vi[i])
     {
-      dp[j][i] = dp[j-1][i-1] + arr[j];
-      while (!vi.empty())
-        if (arr[j] > arr[vi.back()])
-        {
-          dp[j][i] = min(dp[j][i], dp[vi.back()][i] - arr[vi.back()] + arr[j]);
-          vi.pop_back();
-        }
-        else
-          break;
-      if (!vi.empty())
-        dp[j][i] = min(dp[j][i], dp[vi.back()][i] + arr[j]);
-      vi.push_back(j);
+      cin >> a;
+      all[a].push_back(i);
     }
   }
-  cout << dp[n - 1][k] << "\n";
+  for(int i=0;i<m;i++)
+    for(int j=0;j<m;j++)
+      adjmatrix[i][j] = 1e9;
+  for()
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < all[i].size(); j++)
+      for (int k = j + 1; k < all[i].size(); k++)
+        adjmatrix[all[i][j]][all[i][k]] = 1, adjmatrix[all[i][k]][all[i][j]] = 1;
+  for(int i=0;i<m;i++)
+    for(int j=i+1;j<m;j++)
+      for(int k=0;k<m;k++)
+        if(k!=i && k!=j)
+        {
+         if(adjmatrix[i][k] + adjmatrix[k][j]<)
+        }
 }
