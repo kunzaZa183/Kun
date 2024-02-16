5ffca5a9d696a445f9d1ec3db0deb13478dba672
kunzaZa183 <yongvanichthee@gmail.com>
2024-02-16 20:06:06 +0700

    do oil tmrw

--- main.cpp
+++ main.cpp
@@ -1,39 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxm = 500, maxn = 1000001;
-vector<int> vi[maxm];
-int adjmatrix[maxm][maxm];
-vector<int> all[maxn];
+const int maxn = 1501, logn = 11;
+int arr[maxn][maxn], right[maxn][4 * maxn], down[maxn][4 * maxn], rect[maxn][maxn];
+void buildright(int curx, int curin, int curl, int curr)
+{
+  buildright(curx, curin * 2 + 1, curl, (curl + curr) / 2), buildright(curx, curin * 2 + 2, (curl + curr) / 2 + 1, curr);
+  
+}
 int main()
 {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
-  int n, m;
-  cin >> n >> m;
-  for (int i = 0; i < m; i++)
-  {
-    int x;
-    cin >> x;
-    vi[i].resize(x);
-    for (auto &a : vi[i])
-    {
-      cin >> a;
-      all[a].push_back(i);
-    }
-  }
-  for(int i=0;i<m;i++)
-    for(int j=0;j<m;j++)
-      adjmatrix[i][j] = 1e9;
-  for()
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < all[i].size(); j++)
-      for (int k = j + 1; k < all[i].size(); k++)
-        adjmatrix[all[i][j]][all[i][k]] = 1, adjmatrix[all[i][k]][all[i][j]] = 1;
-  for(int i=0;i<m;i++)
-    for(int j=i+1;j<m;j++)
-      for(int k=0;k<m;k++)
-        if(k!=i && k!=j)
-        {
-         if(adjmatrix[i][k] + adjmatrix[k][j]<)
-        }
+  int n, m, k;
+  cin >> n >> m >> k;
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= m; j++)
+      cin >> arr[i][j];
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= m; j++)
+      arr[i][j] += arr[i][j - 1] + arr[i - 1][j] - arr[i - 1][j - 1];
+  for (int i = k; i <= n; i++)
+    for (int j = k; j <= m; j++)
+      rect[i][j] = arr[i][j] - arr[i - k][j] - arr[i][j - k] + arr[i - k][j - k];
+  for (int i = 0; i <= logn; i++)
 }
