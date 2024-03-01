2bd8058f09db48c7faf220142a86d8fc7bb4c3f8
kunzaZa183 <yongvanichthee@gmail.com>
2024-03-01 18:06:07 +0700

    AC on APIO 2006

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 1502, logn = 11;
+const int maxn = 1502;
 int arr[maxn][maxn], rect[maxn][maxn];
 int pref[maxn][maxn], suf[maxn][maxn];
 int topl[maxn][maxn], topr[maxn][maxn], botr[maxn][maxn], botl[maxn][maxn];
@@ -27,7 +27,7 @@ int main()
       pref[i][j] = max(rect[i][j], pref[i][j - 1]);
   for (int i = 1; i <= n; i++)
     for (int j = m; j >= 1; j--)
-      suf[i][j] = max(rect[i][j], suf[i][j - 1]);
+      suf[i][j] = max(rect[i][j], suf[i][j + 1]);
 
   for (int j = 1; j <= m; j++)
     for (int i = 1; i <= n; i++)
@@ -64,23 +64,26 @@ int main()
 
   int maxi = 0;
 
-  for (int i = k + 1; i <= n; i++)
+  for (int i = k; i <= n; i++)
     for (int j = i + k; j <= n - k; j++)
-      maxi = max(maxi, row2[1][i - 1] + row2[i][j - 1] + row2[j][n]);
+      maxi = max(maxi, row2[1][i] + row2[i + k][j] + row2[j + k][n]);
 
-  for (int i = k + 1; i <= m; i++)
+  for (int i = k; i <= m; i++)
     for (int j = i + k; j <= m - k; j++)
-      maxi = max(maxi, col2[1][i - 1] + col2[i][j - 1] + col2[j][m]);
+      maxi = max(maxi, col2[1][i] + col2[i + k][j] + col2[j + k][m]);
 
-  if (m >= 2 * k && n >= 2 * k)
-  {
-    for (int i = k; i <= n - k; i++)
-      for (int j = k; j <= m - k; j++)
-      {
-        int a = topl[i][j], b = topr[i][j + 1], c = botl[i + 1][j], d = botr[i + 1][j + 1];
-        maxi = max({maxi, a + b + c, a + b + d, a + c + d, b + c + d});
-      }
-  }
+  for (int i = k; i <= n - k; i++)
+    for (int j = k; j <= m - k; j++)
+    {
+      // 2left 1right
+      maxi = max(maxi, topl[i][j] + botl[i + k][j] + col2[j + k][m]);
+      // 1left 2right
+      maxi = max(maxi, col2[1][j] + topr[i][j + k] + botr[i + k][j + k]);
+      // 1up 2down
+      maxi = max(maxi, row2[1][i] + botl[i + k][j] + botr[i + k][j + k]);
+      // 2up 1down
+      maxi = max(maxi, topl[i][j] + topr[i][j + k] +  row2[i + k][n]);
+    }
 
-  cout<<maxi<<"\n";
+  cout << maxi << "\n";
 }
