eb74dd843097ba4af84124ea4cc5bc416dd4b8b0
kunzaZa183 <yongvanichthee@gmail.com>
2024-03-02 14:42:02 +0700

    Upsolve CF

--- main.cpp
+++ main.cpp
@@ -11,13 +11,50 @@ int32_t main()
   {
     int n, m;
     cin >> n >> m;
-    int y = n ^ m;
-    if (y < n)
+    vector<int> vi, target;
+    for (int i = 0; i < 63; i++)
+      if ((1ll << i) & n)
+        vi.push_back(i);
+    for (int i = 0; i < 63; i++)
+      if ((1ll << i) & m)
+        target.push_back(i);
+    if (vi.size() == 1)
     {
-      cout << "1\n";
-      cout << n << ' ' << m << "\n";
+      cout << "-1\n";
+      continue;
+    }
+    if (target.back() == vi.back())
+    {
+      if (target.size() == 1)
+      {
+        cout << "1\n"
+             << n << " " << m << '\n';
+      }
+      else if (target[target.size() - 2] <= vi[vi.size() - 2])
+      {
+        cout << "1\n"
+             << n << " " << m << '\n';
+      }
+      else
+        cout << "-1\n";
+    }
+    else if (target.back() > vi[vi.size() - 2])
+    {
+      cout << "-1\n";
+      continue;
     }
     else
-      cout << "-1\n";
+    {
+      int num = (1ll << (vi[vi.size() - 2] + 1));
+      num--;
+      if (num == m)
+        cout << "1\n"
+             << n << " " << m << "\n";
+      else
+      {
+        cout << "2\n";
+        cout << n << ' ' << num << ' ' << m << "\n";
+      }
+    }
   }
 }
