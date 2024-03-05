cfcc35750bd08fc7662d90aad86d45059b5809dc
kunzaZa183 <yongvanichthee@gmail.com>
2024-03-05 23:28:58 +0700

    4 Problems CF

--- main.cpp
+++ main.cpp
@@ -1,59 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-int32_t main()
+const int maxn = 200000;
+pair<int,int> arr[maxn];
+vector<int> all[30][maxn];
+int main()
 {
   cin.tie(0)->sync_with_stdio(0);
   cin.exceptions(cin.failbit);
   int tests;
-  cin >> tests;
-  while (tests--)
+  cin>>tests;
+  while(tests--)
   {
-    int n, m;
-    cin >> n >> m;
-    vector<int> vi, target;
-    for (int i = 0; i < 63; i++)
-      if ((1ll << i) & n)
-        vi.push_back(i);
-    for (int i = 0; i < 63; i++)
-      if ((1ll << i) & m)
-        target.push_back(i);
-    if (vi.size() == 1)
+    for(int i=0;i<30;i++)
+      all[i].clear();
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+      cin>>arr[i].first>>arr[i].second;
+    for(int i=0;i<n;i++)
     {
-      cout << "-1\n";
-      continue;
-    }
-    if (target.back() == vi.back())
-    {
-      if (target.size() == 1)
+      int tmp = arr[i].second;
+      for(int j=0;j<30;j++)
+      if(arr[j].second & (1<<j))
       {
-        cout << "1\n"
-             << n << " " << m << '\n';
-      }
-      else if (target[target.size() - 2] <= vi[vi.size() - 2])
-      {
-        cout << "1\n"
-             << n << " " << m << '\n';
-      }
-      else
-        cout << "-1\n";
-    }
-    else if (target.back() > vi[vi.size() - 2])
-    {
-      cout << "-1\n";
-      continue;
-    }
-    else
-    {
-      int num = (1ll << (vi[vi.size() - 2] + 1));
-      num--;
-      if (num == m)
-        cout << "1\n"
-             << n << " " << m << "\n";
-      else
-      {
-        cout << "2\n";
-        cout << n << ' ' << num << ' ' << m << "\n";
+        all[i]
       }
     }
   }
